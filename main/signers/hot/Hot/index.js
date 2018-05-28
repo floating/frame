09a5457714ddf7933200bb613f104b69674560f0
Jordan Muir <cjm712@gmail.com>
2018-05-28 00:08:34 -0400

    Hot signers for dev, nonce bump, error handling, tests (#45)
    * Add custom hot signers for development
    * Nonce bump, error handling, tests
    * Update README.md

--- main/signers/hot/Hot/index.js
+++ main/signers/hot/Hot/index.js
@@ -1,20 +1,25 @@
+const Web3 = require('web3')
 const EthereumTx = require('ethereumjs-tx')
 const Signer = require('../../Signer')
 
+let normalize = key => key == null ? '' : key.startsWith('0x') ? key : '0x' + key
+
 class Hot extends Signer {
-  constructor (id) {
+  constructor (key) {
     super()
-    this.id = id
+    this.web3 = new Web3()
+    this.account = this.web3.eth.accounts.privateKeyToAccount(normalize(key))
+    this.id = key
     this.type = 'Hot'
-    this.accounts = [id === 1 ? '0x0559fB375De2281b3C32020d26cc976c53527484' : '0x030E6aF4985f111c265ee3A279e5a9f6AA124Fd5']
-    this.privateKey = id === 1 ? '2d6945dbddb8dcf5492004e6f720f8e971196ff61a61c4be99714ebc71e06c00' : 'aef6a68a47c1628081e4e6df195f5f712ae4eb7da332a6d74dca06ae32a3e7ae'
+    this.accounts = [this.account.address]
+    this.privateKey = key.startsWith('0x') ? key.substring(2) : key
     this.status = 'ok'
     this.open()
   }
   signTransaction (rawTx, cb) {
     const tx = new EthereumTx(rawTx)
     tx.sign(Buffer.from(this.privateKey, 'hex'))
-    setTimeout(() => cb(null, '0x' + tx.serialize().toString('hex')), 1200) // Response delay for development
+    setTimeout(() => cb(null, '0x' + tx.serialize().toString('hex')), 200) // Response delay for development
   }
 }
 
--- main/signers/hot/index.js
+++ main/signers/hot/index.js
@@ -1,18 +1,12 @@
 const Hot = require('./Hot')
 
-const dev = process.env.NODE_ENV === 'development'
-
 module.exports = signers => {
-  if (dev) {
-    signers[1] = new Hot(1)
-    signers[2] = new Hot(2)
-    // signers[3] = new Hot(3)
-    // signers[4] = new Hot(4)
-    // signers[5] = new Hot(5)
-    // signers[6] = new Hot(6)
-    // signers[7] = new Hot(7)
-    // signers[8] = new Hot(8)
-    // signers[9] = new Hot(9)
-    // signers[10] = new Hot(10)
-  }
+  try {
+    let hot = require('../../../hot.json')
+    if (hot && hot.accounts) {
+      hot.accounts.forEach(key => {
+        if (key != null) signers[key] = new Hot(key)
+      })
+    }
+  } catch (e) {}
 }
