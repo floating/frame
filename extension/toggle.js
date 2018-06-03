661d83de33fb29db76adbc0bbb1a0ecc39d542f3
Jordan Muir <cjm712@gmail.com>
2018-06-03 14:29:16 -0400

    Remove extension (#49)

--- extension/toggle.js
+++
@@ -1,6 +0,0 @@
-try {
-  let active = !(JSON.parse(localStorage.getItem('__frameActive')))
-  localStorage.setItem('__frameActive', JSON.stringify(active))
-} catch (e) {
-  console.error('Frame Error:', e)
-}
