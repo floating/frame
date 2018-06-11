cd1b77c4810cf786770e7255f554dc5d539be4db
C. Jordan Muir <cjm712@gmail.com>
2018-06-11 19:14:43 -0400

    Fix menu hide

--- app/provider/index.js
+++ app/provider/index.js
@@ -1,4 +1,18 @@
 import Provider from './Provider'
-// ws://localhost:8545
 // wss://rinkeby.infura.io/_ws
-export default new Provider()
+// ws://localhost:8546
+const provider = new Provider('wss://rinkeby.infura.io/ws')
+
+let checkSync = () => {
+  provider.send({"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}, (res) => {
+    if (!res.error && !res.result) return
+    let startBlock = parseInt(res.result.startingBlock, 16)
+    let endBlock = parseInt(res.result.highestBlock, 16)
+    let currentBlock = parseInt(res.result.currentBlock, 16)
+    let percentDone = Math.round((currentBlock / (startBlock = endBlock)) * 100) / 100
+    console.log('Syncing: ' + perrcentDone + '%')
+    setTimeout(checkSync, 3000)
+  })
+}
+
+export default provider
