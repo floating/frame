d685d0afb03c6f8bc412946447d026d0c2ee70b4
Jordan Muir <cjm712@gmail.com>
2018-06-12 13:36:24 -0400

    HTTP fallback, eth_pollSubscriptions, personal_sign, personal_ecRecover (#58)
    * Initial HTTP fallback
    * Adds eth_pollSubscriptions, personal_sign, personal_ecRecover

--- app/index.js
+++ app/index.js
@@ -6,14 +6,14 @@ import { ipcRenderer } from 'electron'
 import App from './App'
 import Panel from './App/Panel'
 
-import ws from './ws'
+import api from './api'
 import webview from './webview'
 import store from './store'
 
 import './style'
 
 let tray = process.env.FRAME_TYPE === 'tray'
-tray ? ws() : webview()
+tray ? api() : webview()
 let Frame = Restore.connect(tray ? Panel : App, store)
 ReactDOM.render(<Frame />, document.getElementById('frame'))
 
