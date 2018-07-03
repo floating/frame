7f78d916b3648f02fe3d8727f155f08d3331334b
C. Jordan Muir <cjm712@gmail.com>
2018-07-03 17:09:26 -0400

    Improve hot loading

--- app/index.js
+++ app/index.js
@@ -10,6 +10,13 @@ import api from './api'
 import webview from './webview'
 import store from './store'
 
+if (process.env.NODE_ENV === 'development') {
+  const electronHot = require('electron-hot-loader')
+  electronHot.install()
+  electronHot.watchJsx(['../bundle/*.js'])
+  electronHot.watchCss(['../bundle/*.css'])
+}
+
 process.env.FRAME_TYPE = 'tray' // fix later
 let tray = process.env.FRAME_TYPE === 'tray'
 tray ? api() : webview()
