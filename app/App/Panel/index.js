504bb590de665bd5052b7ea9953301c09cc8d7f4
C. Jordan Muir <cjm712@gmail.com>
2018-08-07 15:35:39 -0400

    Simplify menu

--- app/App/Panel/index.js
+++ app/App/Panel/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import Restore from 'react-restore'
 import octicons from 'octicons'
 
@@ -9,10 +8,6 @@ import Local from './Local'
 const networks = {1: 'Mainnet', 3: 'Ropsten', 4: 'Rinkeby', 42: 'Kovan'}
 
 class Panel extends React.Component {
-  constructor (...args) {
-    super(...args)
-    this.state = {scroll: 0}
-  }
   indicator (connection) {
     let status = [connection.local.status, connection.secondary.status]
     if (status.indexOf('connected') > -1) {
@@ -24,8 +19,8 @@ class Panel extends React.Component {
   render () {
     let open = this.store('tray.open')
     return (
-      <div id='panel' onScroll={e => this.setState({scroll: ReactDOM.findDOMNode(e.target).scrollTop})} style={{transform: open ? 'translate3d(0px, 0px, 0px)' : 'translate3d(370px, 0px, 0px)'}}>
-        <div className='panelMenu' style={{opacity: this.store('signer.current') || (this.state.scroll < 50) ? 1 : 0}}>
+      <div id='panel' style={{transform: open ? 'translate3d(0px, 0px, 0px)' : 'translate3d(370px, 0px, 0px)'}}>
+        <div className='panelMenu'>
           <div className='panelDetail'>
             <div className='panelDetailIndicator'>
               {this.indicator(this.store('local.connection'))}
