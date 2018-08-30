43ed9dc9a4a963d8ff0a4139a316aba9eee7b575
C. Jordan Muir <cjm712@gmail.com>
2018-08-30 12:07:01 -0400

    Fix for origin spoofing due to overflowing text not wrapped

--- app/api/trusted.js
+++ app/api/trusted.js
@@ -1,9 +1,12 @@
 import store from '../store'
 import uuidv5 from 'uuid/v5'
 
+const invalidOrigin = o => o !== o.replace(/[^0-9a-z/:.]/gi, '')
+
 export default origin => {
   if (!(store('local.connection.local.status') === 'connected' || store('local.connection.secondary.status') === 'connected')) return false
   if (!origin || origin === 'null') origin = 'Unknown' // Permission granted to unknown origins only persist until the Frame is closed, they are not permanent
+  if (invalidOrigin(origin)) return false
   let permissions = store('local.accounts', store('signer.accounts', 0), 'permissions') || {}
   let perms = Object.keys(permissions).map(id => permissions[id])
   let permIndex = perms.map(p => p.origin).indexOf(origin)
