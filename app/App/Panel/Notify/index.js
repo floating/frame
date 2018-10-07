17c2139533b7137722e5bf4646c37ae96fe26c2c
C. Jordan Muir <cjm712@gmail.com>
2018-10-07 18:59:36 -0400

    Update mainnet enable

--- app/App/Panel/Notify/index.js
+++ app/App/Panel/Notify/index.js
@@ -5,16 +5,17 @@ import Restore from 'react-restore'
 class Notify extends React.Component {
   render () {
     return (
-      <div className={this.store('view.showMainnetWarning') ? 'notify notifyOn' : 'notify'}>
-        <div className='notifyBox'>
+      <div className={this.store('view.showMainnetWarning') ? 'notify notifyOn' : 'notify'} onMouseDown={() => this.store.showMainnetWarning(false)}>
+        <div className='notifyBox' onMouseDown={e => e.stopPropagation()}>
           <div className='notifyTitle'>
             {'Mainnet Warning'}
           </div>
           <div className='notifyBody'>
-            {'Frame is alpha software not ready for mainnet use. If you proceed, be sure to verify the details of every transacion on your device.'}
+            <div className='notifyBodyLine'>{'Frame is alpha software, be cautious when using it on the mainnet and verify all transaction details on your signing device.'}</div>
+            <div className='notifyBodyLine'>{'Proceeed only if you understand the risks.'}</div>
           </div>
           <div className='notifyInput'>
-            <div className='notifyInputOption notifyInputDeny' onMouseDown={() => this.store.showMainnetWarning(false)}>{'Cancel'}</div>
+            <div className='notifyInputOption notifyInputDeny' onMouseDown={() => this.store.showMainnetWarning(false)}>{'Go Back'}</div>
             <div className='notifyInputOption notifyInputApprove' onMouseDown={() => this.store.showMainnetWarning(false).selectNetwork('1').enableMainnet()}>{'Proceed'}</div>
           </div>
         </div>
