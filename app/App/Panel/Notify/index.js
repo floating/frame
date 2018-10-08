987adfe0cfe6f0ad4af53d504a8689a15a63145f
C. Jordan Muir <cjm712@gmail.com>
2018-10-07 22:45:18 -0400

    Update notice style

--- app/App/Panel/Notify/index.js
+++ app/App/Panel/Notify/index.js
@@ -6,17 +6,26 @@ class Notify extends React.Component {
   render () {
     return (
       <div className={this.store('view.showMainnetWarning') ? 'notify notifyOn' : 'notify'} onMouseDown={() => this.store.showMainnetWarning(false)}>
-        <div className='notifyBox' onMouseDown={e => e.stopPropagation()}>
-          <div className='notifyTitle'>
-            {'Mainnet Warning'}
-          </div>
-          <div className='notifyBody'>
-            <div className='notifyBodyLine'>{'Frame is alpha software, be cautious when using it on the mainnet and verify all transaction details on your signing device.'}</div>
-            <div className='notifyBodyLine'>{'Proceeed only if you understand the risks.'}</div>
-          </div>
-          <div className='notifyInput'>
-            <div className='notifyInputOption notifyInputDeny' onMouseDown={() => this.store.showMainnetWarning(false)}>{'Go Back'}</div>
-            <div className='notifyInputOption notifyInputApprove' onMouseDown={() => this.store.showMainnetWarning(false).selectNetwork('1').enableMainnet()}>{'Proceed'}</div>
+        <div className='notifyBoxWrap'>
+          <div className='notifyBox' onMouseDown={e => e.stopPropagation()}>
+            <div className='notifyTitle'>
+              {'Mainnet Notice'}
+            </div>
+            <div className='notifyBody'>
+              <div className='notifyBodyLine'>{'Frame is still alpha software, be cautious using it on the mainnet, verify all transactions on your signing device.'}</div>
+              <div className='notifyBodyLine'>{'Proceeed only if you understand and accept these risks.'}</div>
+            </div>
+            <div className='notifyInput'>
+              <div className='notifyInputOption notifyInputDeny' onMouseDown={() => this.store.showMainnetWarning(false)}>
+                <div className='notifyInputOptionText'>{'Go Back'}</div>
+              </div>
+              <div className='notifyInputOption notifyInputProceed' onMouseDown={() => {
+                this.store.showMainnetWarning(false).enableMainnet()
+                setTimeout(() => this.store.selectNetwork('1'), 700)
+              }}>
+                <div className='notifyInputOptionText'>{'Proceed'}</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
