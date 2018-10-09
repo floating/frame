583feb0b6ea089045ceb080ed7a808627e397a6a
C. Jordan Muir <cjm712@gmail.com>
2018-10-09 07:17:00 -0400

    Update extension ids and check

--- main/api/isExtension.js
+++ main/api/isExtension.js
@@ -1,10 +1,12 @@
 const dev = process.env.NODE_ENV === 'development'
 
-const extOrigins = ['chrome-extension://adpbaaddjmehiidelapmmnjpmehjiifg', 'moz-extension://7b2a0cf9-245a-874b-8f58-4a7e3d04c70f']
+const extOrigins = ['chrome-extension://adpbaaddjmehiidelapmmnjpmehjiifg', 'moz-extension://6b58ce8e-d95d-2e43-8451-03a8970e6e78']
 
 module.exports = origin => {
-  // For dev
-  if (origin.indexOf('chrome-extension://') > -1 || origin.indexOf('moz-extension://') > -1) return true
-  if (extOrigins.indexOf(origin) > -1) return true
+  if (dev) {
+    if (origin.indexOf('chrome-extension://') > -1 || origin.indexOf('moz-extension://') > -1) return true
+  } else {
+    if (extOrigins.indexOf(origin) > -1) return true
+  }
   return false
 }
