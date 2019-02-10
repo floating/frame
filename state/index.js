d372a1017cb50fa26a36f2c803130170115284e0
Jordan Muir <cjm712@gmail.com>
2019-02-10 15:49:58 -0500

    Support new testnet presets (#128)

--- state/index.js
+++ state/index.js
@@ -69,7 +69,7 @@ let initial = {
     accounts: main('accounts', {}), // Persisted account settings and permissions
     connection: {
       network: main('connection.network', '4'),
-      options: ['1', '4'],
+      options: ['1', '3', '4', '42'],
       status: 'loading',
       local: {
         on: main('connection.local.on', false),
@@ -84,7 +84,19 @@ let initial = {
               direct: 'direct'
             }
           },
+          '3': {
+            current: 'direct',
+            options: {
+              direct: 'direct'
+            }
+          },
           '4': {
+            current: 'direct',
+            options: {
+              direct: 'direct'
+            }
+          },
+          '42': {
             current: 'direct',
             options: {
               direct: 'direct'
@@ -101,11 +113,25 @@ let initial = {
               custom: main('connection.secondary.settings.1.options.custom', '')
             }
           },
+          '3': {
+            current: main('connection.secondary.settings.3.current', 'infura'),
+            options: {
+              infura: 'infuraRopsten',
+              custom: main('connection.secondary.settings.3.options.custom', '')
+            }
+          },
           '4': {
             current: main('connection.secondary.settings.4.current', 'infura'),
             options: {
               infura: 'infuraRinkeby',
               custom: main('connection.secondary.settings.4.options.custom', '')
+            }
+          },
+          '42': {
+            current: main('connection.secondary.settings.42.current', 'infura'),
+            options: {
+              infura: 'infuraKovan',
+              custom: main('connection.secondary.settings.42.options.custom', '')
             }
           }
         },
