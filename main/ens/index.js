17a7661a24cc972cbf30a08d8aed1f0237b6a523
Philip Prophet <philip.prophet@gmail.com>
2019-05-02 17:46:42 +0200

    White space reintroduced

--- main/ens/index.js
+++ main/ens/index.js
@@ -12,56 +12,77 @@ const registryAddresses = require('./artifacts/addresses')
 
 /* PUBLIC */
 exports.resolveName = async (name) => {
+  
   // Get resolver address
   const resolverAddress = await getResolverAddress(name)
+  
   // If no resolver found -> return null
   if (!resolverAddress) return null
+  
   // Encode function input
   const node = namehash.hash(name)
   const input = codec.encodeInput(interfaces.resolver, 'addr', [node])
+  
   // Make JSON RPC call
   const params = { to: resolverAddress, data: input }
   const output = await makeCall('eth_call', params)
+  
   // If output empty -> return null
   if (output === '0x') return null
+  
   // Decode output and return value
   const decodedOutput = codec.decodeOutput(interfaces.resolver, 'addr', output)
   return decodedOutput[0]
+
 }
 
 exports.resolveAddress = async (address) => {
+  
   // Construct name
   const name = `${address.slice(2)}.addr.reverse`
+  
   // Get resolver address
   const resolverAddress = await getResolverAddress(name)
+  
   // If no resolver found -> return null
   if (!resolverAddress) return null
+  
   // Encode function input
   const node = namehash.hash(name)
   const input = codec.encodeInput(interfaces.resolver, 'name', [node])
+  
   // Make JSON RPC call
   const params = { to: resolverAddress, data: input }
   const output = await makeCall('eth_call', params)
+  
   // If output empty -> return null
   if (output === '0x') return null
+  
   // Decode output and return value
   const decodedOutput = codec.decodeOutput(interfaces.resolver, 'name', output)
   return decodedOutput[0]
+
 }
 
 exports.resolveContent = async (name) => {
+  
   // Get resolver address
   const resolverAddress = await getResolverAddress(name)
+  
   // If no resolver found -> return null
   if (!resolverAddress) return null
+  
   // Encode function input
   const node = namehash.hash(name)
   const input = codec.encodeInput(interfaces.resolver, 'contenthash', [node])
+  
   // Make JSON RPC call
   const params = { to: resolverAddress, data: input }
   const output = await makeCall('eth_call', params)
+  
   // If output empty -> return null
   if (output === '0x') return null
+  
   // Decode output and return the content hash in text format
   const decodedOutput = codec.decodeOutput(interfaces.resolver, 'contenthash', output)
   const hash = contentHash.decode(decodedOutput[0])
@@ -69,32 +90,45 @@ exports.resolveContent = async (name) => {
   if (type === 'ipfs-ns') return `ipfs://${hash}`
   if (type === 'swarm-ns') return `bzz://${hash}`
   return null
+
 }
 
 /* PRIVATE */
 const getResolverAddress = async (name) => {
+  
   // Hash name
   const hash = namehash.hash(name)
+  
   // Get registry contract address for selected network
   const networkId = provider.connection.network
   const registryAddress = registryAddresses[networkId]
+  
   // Encode function input
   const input = codec.encodeInput(interfaces.registry, 'resolver', [hash])
+  
   // Make JSON RPC call
   const params = { to: registryAddress, data: input }
   const output = await makeCall('eth_call', params)
+  
   // If output empty -> return null
   if (output === '0x') return null
+  
   // Decode output and return value
   const decodedOutput = codec.decodeOutput(interfaces.registry, 'resolver', output)
   return decodedOutput[0]
+
 }
 
 const makeCall = (method, params) => {
+
   return new Promise((resolve, reject) => {
+  
     // Construct JSON RPC payload
     const payload = { jsonrpc: '2.0', id: 1, method: method, params: [params, 'latest'] }
+  
     // Send payload to provider and resolve promise with result
     provider.send(payload, ({ result }) => resolve(result))
+  
   })
+
 }
