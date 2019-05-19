0b20c0fb762d4e4a75695331296ae0c6b5443dc3
Philip Prophet <philip.prophet@gmail.com>
2019-05-19 11:18:51 +0200

    Switches to Jest and adds assertion counter

--- test/clients/geth.test.js
+++ test/clients/geth.test.js
@@ -6,76 +6,106 @@ const { emptyDir } = require('fs-extra')
 const assert = require('assert')
 const EventEmitter = require('events')
 
-const userData = path.resolve('./test/clients/userData')
+const userData = path.resolve('./test/.userData')
 
-const clean = async () => await emptyDir(userData)
-
+// TODO: Move to util directory (and talk to Jordan about this setup)
 class Observer extends EventEmitter {
-  constructor(key) {
+  constructor(root, keys) {
     super()
+
+    // Setup root observer
+    this.root = new EventEmitter()
     store.observer(_ => {
-      const value = store(key)
+      const value = store(root)
       this.emit('change', value)
+    })
+
+    // Setup key observers
+    keys.forEach((key) => {
+      this[key] = new EventEmitter()
+      store.observer(_ => {
+        const value = store(`${root}.${key}`)
+        this[key].emit('change', value)
+      })
     })
   }
 }
 
-describe('Install and uninstall client', function (done) {
 
-  const observer = new Observer('main.clients.geth')
+class Counter {
+  constructor(number, done) {
+    this.count = 0
+    this.number = number
+    this.done = done
+  }
+
+  expect (value) {
+    this.count++
+    setTimeout(() => {
+      if (this.count === this.number) {
+        this.done()
+      }
+    }, 1)
+    return expect(value)
+  }
+
+}
+
+const observer = new Observer('main.clients.geth', ['state', 'installed', 'latest', 'version'])
+const clean = async () => await emptyDir(userData)
+
+describe('Installing and uninstalling geth', () => { 
 
   // BEFORE: Make sure test user data directory is empty
-  before(clean)
+  beforeAll(clean)
+  afterAll(clean)
 
-  it('Client should not be installed', function() {
+  test('Install geth', function(done) {
+    const counter = new Counter(4, done)
     // ASSERT: User data directory is empty
     const files = fs.readdirSync(userData)
-    assert.equal(files.length, 0)
+    counter.expect(files.length).toEqual(0)
 
     // ASSERT: Store reflects uninstalled client
     const { latest, installed, state } = store('main.clients.geth')
-    assert.equal(latest, false)
-    assert.equal(installed, false)
-    assert.equal(state, 'off')
+    counter.expect(latest).toBe(false)
+    counter.expect(installed).toBe(false)
+    counter.expect(state).toBe('off')
   })
 
-  it('Client should install', function (done) {
-
-    this.timeout(30000)
-
-    // Begin installation
-    observer.once('change', (data) => {
-      assert.equal(data.state, 'installing')
-
-      // Installation done
-      observer.once('change', (data) => {
-        assert.equal(data.installed, true)
-        assert.equal(data.latest, true)
-        assert.notEqual(data.version, null)
-        done()
-      })
+  test('Client should install', function (done) {
+    // Setup test
+    const counter = new Counter(5, done)
+    jest.setTimeout(30000)
+               
+    // On installing -> assert state change
+    observer.state.once('change', (value) => {
+      counter.expect(value).toBe('installing')
+       // On install done -> assert state change
+      observer.installed.once('change', (value) => counter.expect(value).toBe(true))
+      observer.latest.once('change', (value) => counter.expect(value).toBe(true))
+      observer.version.once('change', (value) => counter.expect(typeof(value)).toBe('string'))
+      observer.state.once('change', (value) => counter.expect(value).toBe('off'))
     })
 
+    // observer.state.on('change', console.log)
     // Run install process
     geth.install()
   })
 
-  it('Client should uninstall', function (done) {
+  test('Client should uninstall', function (done) {
+    const counter = new Counter(4, done)
+    
     observer.once('change', (data) => {
       // ASSERT: Store reflects uninstalled client
-      assert.equal(data.installed, false)
-      assert.equal(data.latest, false)
-      assert.equal(data.state, 'off')
+      counter.expect(data.installed).toBe(false)
+      counter.expect(data.latest).toBe(false)
+      counter.expect(data.state).toBe('off')
       // ASSERT: User data directory is empty
       const files = fs.readdirSync(userData)
-      assert.equal(files.length, 0)
-      done()
+      counter.expect(files.length).toBe(0)
     })
     // Run uninstall process
     geth.uninstall()
   })
-
-  // AFTER: Clean up test user data directory
-  after(clean)
-
 })
