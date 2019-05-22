a1cddd0dbb596b76a15f40a1ed6f3b102357f1df
Philip Prophet <philip.prophet@gmail.com>
2019-05-22 06:35:28 +0200

    Completes IPFS service

--- main/services/ipfs.js
+++ main/services/ipfs.js
@@ -3,57 +3,41 @@ const store = require('../store')
 const log = require('electron-log')
 
 class IPFS extends Service {
-  constructor (options) {
+  constructor(options) {
     super('ipfs', options)
   }
 
-  start () {
+  start() {
+    // On 'service ready' -> start ipfs
     this.on('ready', async () => {
-
-      // Run ipfs init
-      try {
-        log.info('IPFS initiated')
-        await this._runOnce(['ps'])
-      }
-      catch(err) { log.info('ipfs: already initiated') }
-
-      // Wait for daemon to get ready
-      // TODO: Swtich to stream!
-      this.on('stdout', (stdout) => {
-        console.log(stdout)
-        if (stdout.includes('Daemon is ready')) {
-          log.info('ipfs: ready')
-          store.setClientState('ipfs', 'ready')
-        }
-      })
-
-      // Start daemon
+      // Run 'ipfs init'
+      this.init()
+      // Run 'ipfs daemon'
       this._run(['daemon'])
-
-      // // Prepare client arguments
-      // let args = ['--networkid', networkId, '--syncmode', mode, '--nousb', '--rpc']
-      // if (networkFlag) args.push(networkFlag)
-
-      // // Start client
-      // this._run(args)
-
     })
+
+    // On 'daemon ready' -> switch client state
+    this.on('stdout', (stdout) => {
+      console.log(stdout)
+      if (stdout.includes('Daemon is ready')) {
+        log.info('ipfs: ready')
+        store.setClientState('ipfs', 'ready')
+      }
+    })
+
+    // Run start sequence
     this._start()
   }
 
-  stop () {
-    // Terminate service
-    this._stop()
+  stop() { this._stop() }
+
+  async init() {
+    try {
+      log.info('IPFS initiated')
+      await this._runOnce(['init'])
+    }
+    catch (err) { log.info('ipfs: already initiated') }
   }
 }
 
 module.exports = new IPFS()
-
-// DEBUG
-const ipfs = new IPFS({ log: false })
-ipfs.start()
-setTimeout(() => {
-  ipfs.stop()
-}, 10000);
-
-store.observer(_ => console.log(store('main.clients.ipfs')))
