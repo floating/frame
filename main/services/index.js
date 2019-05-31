24d43237b087e977412d957059b076c8ea1613f4
Philip Prophet <philip.prophet@gmail.com>
2019-05-31 10:33:22 +0200

    Links geth to local connection and global network setting

--- main/services/index.js
+++ main/services/index.js
@@ -3,16 +3,37 @@ const ipfs = require('./ipfs')
 const store = require('../store')
 const { app } = require('electron')
 
+// Ethereum
 app.on('ready', () => {
   let on = null
+  let network = store('main.connection.network')
   store.observer(_ => {
+
+    // If client toggled ->
     if (on !== store('main.clients.geth.on')) {
+      // Update holder variable
       on = store('main.clients.geth.on')
-      on ? geth.start() : geth.stop()
+      // If toggled on -> start geth and toggle on local connection
+      if (on) {
+        geth.start()
+        if (!store('main.connection.local.on')) setTimeout(() => store.toggleConnection('local'), 1000)
+      // Else -> stop geth and toggle off local connection
+      } else {
+        if (store('main.connection.local.on')) store.toggleConnection('local')
+        setTimeout(() => geth.stop(), 1000)
+      }
     }
+
+    if (network !== store('main.connection.network') && store('main.clients.geth.on')) {
+      network = store('main.connection.network')
+      store.toggleClient('geth')
+      geth.once('exit', () => store.toggleClient('geth'))
+    }
+
   })
 })
 
+// IPFS
 app.on('ready', () => {
   let on = null
   store.observer(_ => {
