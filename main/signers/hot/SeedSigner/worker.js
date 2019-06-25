8dcc3c6bd96a10de2df6c3fb4cdd8aaf73ed7024
Philip Prophet <philip.prophet@gmail.com>
2019-06-25 09:13:40 +0200

    cb -> pseudoCallback (to pass linting)

--- main/signers/hot/SeedSigner/worker.js
+++ main/signers/hot/SeedSigner/worker.js
@@ -7,24 +7,24 @@ const crypt = require('../../../crypt')
 
 let seed = null
 
-const unlockAccount = async ({ encryptedSeed, password }, cb) => {
+const unlockAccount = async ({ encryptedSeed, password }, pseudoCallback) => {
   crypt.decrypt(encryptedSeed, password, (err, decryptedSeed) => {
-    if (err) cb('Invalid password')
+    if (err) pseudoCallback('Invalid password')
     else {
       seed = decryptedSeed
-      cb(null, 'ok')
+      pseudoCallback(null, 'ok')
     }
   })
 }
 
-const lockAccount = async (cb) => {
+const lockAccount = async (pseudoCallback) => {
   seed = null
-  cb()
+  pseudoCallback()
 }
 
-const signMessage = async ({ index, message }, cb) => {
+const signMessage = async ({ index, message }, pseudoCallback) => {
   // Make sure account is unlocked
-  if (!seed) return cb('Account locked')
+  if (!seed) return pseudoCallback('Account locked')
   // Hash message
   const hash = hashPersonalMessage(toBuffer(message))
   // Derive private key
@@ -33,12 +33,12 @@ const signMessage = async ({ index, message }, cb) => {
   const signed = ecsign(hash, pk)
   // Return serialized signed message
   const hex = Buffer.concat([Buffer.from(signed.r), Buffer.from(signed.s), Buffer.from([signed.v])]).toString('hex')
-  cb(null, addHexPrefix(hex))
+  pseudoCallback(null, addHexPrefix(hex))
 }
 
-const signTransaction = async ({ index, rawTx }, cb) => {
+const signTransaction = async ({ index, rawTx }, pseudoCallback) => {
   // Make sure account is unlocked
-  if (!seed) return cb('Account locked')
+  if (!seed) return pseudoCallback('Account locked')
   // Create tranasction
   const tx = new EthTx(rawTx)
   // Derive private key
@@ -46,12 +46,12 @@ const signTransaction = async ({ index, rawTx }, cb) => {
   // Sign transaction
   tx.sign(pk)
   // Return serialized transaction
-  cb(null, '0x' + tx.serialize().toString('hex'))
+  pseudoCallback(null, '0x' + tx.serialize().toString('hex'))
 }
 
-const verifyAddress = async ({ index, address }, cb) => {
+const verifyAddress = async ({ index, address }, pseudoCallback) => {
   // Make sure account is unlocked
-  if (!seed) return cb('Account locked')
+  if (!seed) return pseudoCallback('Account locked')
   // Construct message
   const message = uuid()
   // Sign message
@@ -68,27 +68,27 @@ const verifyAddress = async ({ index, address }, cb) => {
   const hash = hashPersonalMessage(toBuffer(message))
   const verifiedAddress = '0x' + pubToAddress(ecrecover(hash, v, r, s)).toString('hex')
   // Return result
-  cb(null, verifiedAddress.toLowerCase() === address.toLowerCase())
+  pseudoCallback(null, verifiedAddress.toLowerCase() === address.toLowerCase())
 }
 
 process.on('message', async ({ id, method, params }) => {
-  // Define callback
-  const cb = (error, result) => {
+  // Define (pseudo) callback
+  const pseudoCallback = (error, result) => {
     // Add correlation id to response
     let response = { id, error, result }
     // Send response to parent process
     process.send(response)
   }
   // Handle method 'unlockAccount'
-  if (method === 'unlockAccount') return unlockAccount(params, cb)
+  if (method === 'unlockAccount') return unlockAccount(params, pseudoCallback)
   // Handle method 'unlockAccount'
-  if (method === 'lockAccount') return lockAccount(cb)
+  if (method === 'lockAccount') return lockAccount(pseudoCallback)
   // Handle method 'signMessage'
-  if (method === 'signMessage') return signMessage(params, cb)
+  if (method === 'signMessage') return signMessage(params, pseudoCallback)
   // Handle method 'signTransaction'
-  if (method === 'signTransaction') return signTransaction(params, cb)
+  if (method === 'signTransaction') return signTransaction(params, pseudoCallback)
   // Handle method 'verifyAddress'
-  if (method === 'verifyAddress') return verifyAddress(params, cb)
+  if (method === 'verifyAddress') return verifyAddress(params, pseudoCallback)
   // Handle invalid method
-  else cb(`Invalid method: '${method}'`)
+  else pseudoCallback(`Invalid method: '${method}'`)
 })
