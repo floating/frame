2a7f96a9a77b579c03b143df071525fdefc0b711
Philip Prophet <philip.prophet@gmail.com>
2019-07-04 15:35:03 +0200

    Refactors 'hot' module and seed signer creation

--- main/signers/hot/SeedSigner/index.js
+++ main/signers/hot/SeedSigner/index.js
@@ -1,13 +1,44 @@
 const path = require('path')
 const HotSigner = require('../HotSigner')
+const bip39 = require('bip39')
+const hdKey = require('ethereumjs-wallet/hdkey')
 
 const WORKER_PATH = path.resolve(__dirname, 'worker.js')
 
 class SeedSigner extends HotSigner {
   constructor (signer) {
     super(signer, WORKER_PATH)
-    this.encryptedSeed = signer.encryptedSeed
+    this.encryptedSeed = (signer && signer.encryptedSeed)
+    this.type = 'seed'
     this.update()
+  }
+
+  addSeed (seed, password, cb) {
+    if (this.encryptedSeed) return cb(new Error('This signer already has a seed'))
+
+    this._callWorker({ method: 'encryptSeed', params: { seed, password } }, (err, encryptedSeed) => {
+      if (err) return cb(err)
+
+      // Derive addresses
+      const wallet = hdKey.fromMasterSeed(Buffer.from(seed, 'hex')).derivePath('m/44\'/60\'/0\'/0')
+      const addresses = []
+      for (var i = 0; i < 100; i++) { addresses.push(wallet.deriveChild(i).getWallet().getChecksumAddressString()) }
+
+      // Update signer
+      this.encryptedSeed = encryptedSeed
+      this.addresses = addresses
+
+      cb(null)
+    })
+  }
+
+  async addPhrase (phrase, password, cb) {
+    // Validate phrase
+    if (!bip39.validateMnemonic(phrase)) return cb(new Error('Invalid mnemonic phrase'))
+    // Generate seed
+    const seed = await bip39.mnemonicToSeed(phrase)
+    // Add seed to signer
+    this.addSeed(seed.toString('hex'), password, cb)
   }
 
   save () {
@@ -17,6 +48,7 @@ class SeedSigner extends HotSigner {
   unlock (password, cb) {
     super.unlock(password, { encryptedSeed: this.encryptedSeed }, cb)
   }
+
 }
 
 module.exports = SeedSigner
