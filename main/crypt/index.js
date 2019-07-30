e944c3900bcbea503944c3386adb1461c241e6c6
C. Jordan Muir <cjm712@gmail.com>
2019-07-30 10:59:20 -0400

    Remove unused encrypt/decrypt

--- main/crypt/index.js
+++ main/crypt/index.js
@@ -5,24 +5,4 @@ const stringToKey = pass => {
   return Buffer.from(hash.digest('hex').substring(0, 32))
 }
 
-const encrypt = (string, pass, cb) => {
-  try {
-    const iv = crypto.randomBytes(16)
-    const cipher = crypto.createCipheriv('aes-256-cbc', stringToKey(pass), iv)
-    const encrypted = Buffer.concat([cipher.update(string), cipher.final()])
-    cb(null, iv.toString('hex') + ':' + encrypted.toString('hex'))
-  } catch (e) { cb(e) }
-}
-
-const decrypt = (string, pass, cb) => {
-  try {
-    const parts = string.split(':')
-    const iv = Buffer.from(parts.shift(), 'hex')
-    const decipher = crypto.createDecipheriv('aes-256-cbc', stringToKey(pass), iv)
-    const encryptedString = Buffer.from(parts.join(':'), 'hex')
-    const decrypted = Buffer.concat([decipher.update(encryptedString), decipher.final()])
-    cb(null, decrypted.toString())
-  } catch (e) { cb(e) }
-}
-
-module.exports = { encrypt, decrypt, stringToKey }
+module.exports = { stringToKey }
