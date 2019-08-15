edf6e486196cacf92e95d5fd6507156836ca278c
Simon Emanuel Schmid <simon@schmid.io>
2019-08-15 15:03:41 +0200

    Fix crash if signers without device are present

--- main/signers/trezor-connect/index.js
+++ main/signers/trezor-connect/index.js
@@ -13,17 +13,17 @@ module.exports = {
       })
       flex.on('trezor:disconnect', device => {
         log.info(':: Trezor Scan - Disconnected Device')
-        const signer = signers.find(signer => signer.device.path === device.path)
+        const signer = signers.find(signer => (signer.device && signer.device.path) === device.path)
         if (signer) signers.remove(signer.id)
       })
       flex.on('trezor:update', device => {
-        log.info(':: Trezor Scan - Updated Device')
-        const signer = signers.find(signer => signer.device.path === device.path)
+        log.info(':: Trezor Scan - Updated Device') null, 2))
+        const signer = signers.find(signer => (signer.device && signer.device.path) === device.path)
         if (signer) signer.update(device)
       })
       flex.on('trezor:needPin', device => {
         log.info(':: Trezor Scan - Device Needs Pin')
-        const signer = signers.find(signer => signer.device.path === device.path)
+        const signer = signers.find(signer => (signer.device && signer.device.path) === device.path)
         if (signer) signer.needPin()
       })
       flex.on('trezor:needPhrase', device => {
