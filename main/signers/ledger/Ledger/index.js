f96b66d5e4b480a8e2bae45495b7795dcca45ded
C. Jordan Muir <cjm712@gmail.com>
2019-08-30 14:55:30 -0400

    Ledger get/release device

--- main/signers/ledger/Ledger/index.js
+++ main/signers/ledger/Ledger/index.js
@@ -5,9 +5,7 @@ const Eth = require('@ledgerhq/hw-app-eth').default
 const HID = require('node-hid')
 const TransportNodeHid = require('@ledgerhq/hw-transport-node-hid').default
 const store = require('../../../store')
-// const windows = require('../../../windows')
 const Signer = require('../../Signer')
-// const { hashTypedData } = require('../../../crypt/typedDataUtils')
 const uuid = require('uuid/v5')
 const ns = '3bbcee75-cecc-5b56-8031-b6641c1ed1f1'
 
@@ -30,6 +28,8 @@ class Ledger extends Signer {
     this.pause = false
     this.coinbase = '0x'
     this.handlers = {}
+    this.inUse = false
+    this.lastUse = Date.now()
     this.network = store('main.connection.network')
     this.networkObserver = store.observer(() => {
       if (this.network !== store('main.connection.network')) {
@@ -67,6 +67,32 @@ class Ledger extends Signer {
     store.updateSigner(this.summary())
   }
 
+  async wait (time) {
+    return new Promise(resolve => setTimeout(resolve, time))
+  }
+
+  async getDevice () {
+    if (this.inUse) throw new Error('Device is in use')
+    if (Date.now() - this.lastUse < 500) await this.wait(500)
+    this.releaseDevice()
+    this.inUse = true
+    this.pause = true
+    this.currentDevice = new HID.HID(this.devicePath)
+    this.currentTransport = new TransportNodeHid(this.currentDevice)
+    return new Eth(this.currentTransport)
+  }
+
+  async releaseDevice () {
+    if (this.currentTransport) this.currentTransport.close()
+    if (this.currentDevice) this.currentDevice.close()
+    delete this.currentTransport
+    delete this.currentDevice
+    this.lastUse = Date.now()
+    await this.wait(500)
+    this.inUse = false
+    this.pause = false
+  }
+
   reset () {
     this.derivation = store('main.ledger.derivation')
     this.network = store('main.connection.network')
@@ -77,14 +103,11 @@ class Ledger extends Signer {
 
   async getDeviceAddress (i, cb) {
     if (this.pause) return cb(new Error('Device access is paused'))
-    this.pause = true
     try {
       const { address } = await this.getAddress(this.getPath(i), false, true)
       cb(null, address)
     } catch (err) {
       cb(err)
-    } finally {
-      this.pause = false
     }
   }
 
@@ -98,13 +121,11 @@ class Ledger extends Signer {
       return cb(new Error('Device access is paused'))
     }
     verifyActive = true
-    this.pause = true
     try {
       const result = await this.getAddress(this.getPath(index), display, true)
       const address = result.address.toLowerCase()
       current = current.toLowerCase()
       if (address !== current) {
-        // TODO: Error Notification
         log.error(new Error('Address does not match device'))
         this.signers.remove(this.id)
         cb(new Error('Address does not match device'))
@@ -118,7 +139,6 @@ class Ledger extends Signer {
       this.signers.remove(this.id)
       cb(new Error('Verify Address Error'))
     } finally {
-      this.pause = false
       verifyActive = false
     }
   }
@@ -126,7 +146,6 @@ class Ledger extends Signer {
   async deriveAddresses () {
     let addresses
     if (this.pause) throw new Error('Device access is paused')
-    this.pause = true
     try {
       // Derive addresses
       if (this.network !== '1' || this.derivation === 'legacy') {
@@ -139,9 +158,7 @@ class Ledger extends Signer {
       // Update signer
       this.addresses = addresses
       this.update()
-      this.pause = false
     } catch (err) {
-      this.pause = false
       throw err
     }
   }
@@ -151,6 +168,7 @@ class Ledger extends Signer {
     if (this._deviceStatus) clearTimeout(this._deviceStatus)
     if (this._signMessage) clearTimeout(this._signMessage)
     if (this._signTransaction) clearTimeout(this._signTransaction)
+    this.releaseDevice()
     this.networkObserver.remove()
     this.derivationObserver.remove()
     store.removeSigner(this.id)
@@ -179,7 +197,6 @@ class Ledger extends Signer {
       this.status = 'ok'
       this.update()
     } catch (err) {
-      if (this.status === 'Deriving Live Addresses') return
       if (err.message.startsWith('cannot open device with path') || err.message === 'Device access is paused' || err.message === 'Invalid channel') { // Device is busy, try again
         clearTimeout(this._deviceStatus)
         if (++this.busyCount > 10) {
@@ -218,27 +235,15 @@ class Ledger extends Signer {
   // Standard Methods
   async signMessage (index, message, cb) {
     if (this.pause) return cb(new Error('Device access is paused'))
-    this.pause = true
     try {
-      const device = new HID.HID(this.devicePath)
-      const transport = new TransportNodeHid(device)
-      const eth = new Eth(transport)
-      eth.signPersonalMessage(this.getPath(index), message.replace('0x', '')).then(result => {
-        let v = (result['v'] - 27).toString(16)
-        if (v.length < 2) v = '0' + v
-        cb(null, '0x' + result['r'] + result['s'] + v)
-        transport.close()
-        device.close()
-        this.busyCount = 0
-        this.pause = false
-      }).catch(err => {
-        cb(err)
-        transport.close()
-        device.close()
-        this.pause = false
-      })
+      const eth = await this.getDevice()
+      const result = await eth.signPersonalMessage(this.getPath(index), message.replace('0x', ''))
+      let v = (result['v'] - 27).toString(16)
+      if (v.length < 2) v = '0' + v
+      cb(null, '0x' + result['r'] + result['s'] + v)
+      await this.releaseDevice()
+      this.busyCount = 0
     } catch (err) {
-      this.pause = false
       if (err.message.startsWith('cannot open device with path')) {
         clearTimeout(this._signMessage)
         if (++this.busyCount > 10) {
@@ -250,92 +255,37 @@ class Ledger extends Signer {
         }
       }
       cb(err)
+      await this.releaseDevice()
       log.error(err)
     }
   }
 
-  /// / NOTE: Commented out because Ledger does not support signTypedData at the moment
-  /// / see: https://github.com/floating/frame/issues/136
-  /// /
-  // async signTypedData (index, typedData, cb) {
-  //   if (this.pause) return cb(new Error('Device access is paused'))
-  //   this.pause = true
-  //   try {
-  //     const device = new HID.HID(this.devicePath)
-  //     const transport = new TransportNodeHid(device)
-  //     // let transport = await TransportNodeHid.open(this.devicePath)
-  //     const eth = new Eth(transport)
-  //     const message = hashTypedData(typedData).toString('hex')
-
-  //     eth.signPersonalMessage(this.getPath(index), message.replace('0x', '')).then(result => {
-  //       let v = (result['v'] - 27).toString(16)
-  //       if (v.length < 2) v = '0' + v
-  //       cb(null, '0x' + result['r'] + result['s'] + v)
-  //       transport.close()
-  //       device.close()
-  //       this.busyCount = 0
-  //       this.pause = false
-  //     }).catch(err => {
-  //       cb(err)
-  //       transport.close()
-  //       device.close()
-  //       this.pause = false
-  //     })
-  //   } catch (err) {
-  //     this.pause = false
-  //     if (err.message.startsWith('cannot open device with path')) {
-  //       clearTimeout(this._signMessage)
-  //       if (++this.busyCount > 10) {
-  //         this.busyCount = 0
-  //         return log.info('>>>>>>> Busy: Limit (10) hit, cannot open device with path, will not try again')
-  //       } else {
-  //         this._signMessage = setTimeout(() => this.signMessage(message, cb), 700)
-  //         return log.info('>>>>>>> Busy: cannot open device with path, will try again (signMessage)')
-  //       }
-  //     }
-  //     cb(err)
-  //     log.error(err)
-  //   }
-  // }
-
   async signTransaction (index, rawTx, cb) {
     if (this.pause) return cb(new Error('Device access is paused'))
-    this.pause = true
     try {
-      const device = new HID.HID(this.devicePath)
-      const transport = new TransportNodeHid(device)
-      const eth = new Eth(transport)
-      if (parseInt(this.network) !== utils.hexToNumber(rawTx.chainId)) return cb(new Error('Signer signTx network mismatch'))
+      const eth = await this.getDevice()
+      if (parseInt(this.network) !== utils.hexToNumber(rawTx.chainId)) throw new Error('Signer signTx network mismatch')
       const tx = new EthereumTx(rawTx)
       tx.raw[6] = Buffer.from([rawTx.chainId]) // v
       tx.raw[7] = Buffer.from([]) // r
       tx.raw[8] = Buffer.from([]) // s
       const rawTxHex = tx.serialize().toString('hex')
-      eth.signTransaction(this.getPath(index), rawTxHex).then(result => {
-        transport.close()
-        device.close()
-        this.busyCount = 0
-        this.pause = false
-        const tx = new EthereumTx({
-          nonce: Buffer.from(this.normalize(rawTx.nonce), 'hex'),
-          gasPrice: Buffer.from(this.normalize(rawTx.gasPrice), 'hex'),
-          gasLimit: Buffer.from(this.normalize(rawTx.gas), 'hex'),
-          to: Buffer.from(this.normalize(rawTx.to), 'hex'),
-          value: Buffer.from(this.normalize(rawTx.value), 'hex'),
-          data: Buffer.from(this.normalize(rawTx.data), 'hex'),
-          v: Buffer.from(this.normalize(result.v), 'hex'),
-          r: Buffer.from(this.normalize(result.r), 'hex'),
-          s: Buffer.from(this.normalize(result.s), 'hex')
-        })
-        cb(null, '0x' + tx.serialize().toString('hex'))
-      }).catch(err => {
-        transport.close()
-        device.close()
-        this.pause = false
-        cb(err.message)
+      const result = await eth.signTransaction(this.getPath(index), rawTxHex)
+      this.busyCount = 0
+      const _tx = new EthereumTx({
+        nonce: Buffer.from(this.normalize(rawTx.nonce), 'hex'),
+        gasPrice: Buffer.from(this.normalize(rawTx.gasPrice), 'hex'),
+        gasLimit: Buffer.from(this.normalize(rawTx.gas), 'hex'),
+        to: Buffer.from(this.normalize(rawTx.to), 'hex'),
+        value: Buffer.from(this.normalize(rawTx.value), 'hex'),
+        data: Buffer.from(this.normalize(rawTx.data), 'hex'),
+        v: Buffer.from(this.normalize(result.v), 'hex'),
+        r: Buffer.from(this.normalize(result.r), 'hex'),
+        s: Buffer.from(this.normalize(result.s), 'hex')
       })
+      cb(null, '0x' + _tx.serialize().toString('hex'))
+      this.releaseDevice()
     } catch (err) {
-      this.pause = false
       if (err.message.startsWith('cannot open device with path')) {
         clearTimeout(this._signTransaction)
         if (++this.busyCount > 10) {
@@ -347,22 +297,20 @@ class Ledger extends Signer {
         }
       }
       cb(err)
+      this.releaseDevice()
       log.error(err)
     }
   }
 
   async getAddress (...args) {
-    const device = new HID.HID(this.devicePath)
-    const transport = new TransportNodeHid(device)
-    const eth = new Eth(transport)
     try {
+      const eth = await this.getDevice()
       const result = await eth.getAddress(...args)
+      await this.releaseDevice()
       return result
     } catch (err) {
+      await this.releaseDevice()
       throw err
-    } finally {
-      device.close()
-      transport.close()
     }
   }
 
@@ -392,3 +340,45 @@ class Ledger extends Signer {
 }
 
 module.exports = Ledger
+
+// const { hashTypedData } = require('../../../crypt/typedDataUtils')
+
+/// / NOTE: Commented out because Ledger does not support signTypedData at the moment
+/// / see: https://github.com/floating/frame/issues/136
+/// /
+// async signTypedData (index, typedData, cb) {
+//   if (this.pause) return cb(new Error('Device access is paused'))
+//   try {
+//     this.currentDevice = new HID.HID(this.devicePath)
+//     this.currentTransport = new TransportNodeHid(this.currentDevice)
+//     // let transport = await TransportNodeHid.open(this.devicePath)
+//     const eth = new Eth(this.currentTransport)
+//     const message = hashTypedData(typedData).toString('hex')
+
+//     eth.signPersonalMessage(this.getPath(index), message.replace('0x', '')).then(result => {
+//       let v = (result['v'] - 27).toString(16)
+//       if (v.length < 2) v = '0' + v
+//       cb(null, '0x' + result['r'] + result['s'] + v)
+//       this.currentTransport.close()
+//       this.currentDevice.close()
+//       this.busyCount = 0
+//     }).catch(err => {
+//       cb(err)
+//       this.currentTransport.close()
+//       this.currentDevice.close()
+//     })
+//   } catch (err) {
+//     if (err.message.startsWith('cannot open device with path')) {
+//       clearTimeout(this._signMessage)
+//       if (++this.busyCount > 10) {
+//         this.busyCount = 0
+//         return log.info('>>>>>>> Busy: Limit (10) hit, cannot open device with path, will not try again')
+//       } else {
+//         this._signMessage = setTimeout(() => this.signMessage(message, cb), 700)
+//         return log.info('>>>>>>> Busy: cannot open device with path, will try again (signMessage)')
+//       }
+//     }
+//     cb(err)
+//     log.error(err)
+//   }
+// }
