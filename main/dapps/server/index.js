651c0801f4ca2a41b4d3f6ad2aa24dd5a66be1a1
Philip Prophet <philip.prophet@gmail.com>
2019-10-11 15:33:12 +0200

    Isolated PoC complete (runs outside Frame)

--- main/dapps/server/index.js
+++ main/dapps/server/index.js
@@ -1,13 +1,27 @@
 const express = require('express')
-const ipfs = require('../../clients/Ipfs')
+// const ipfs = require('../../clients/Ipfs')
+const IPFS = require('ipfs-http-client')
+const ipfs = IPFS('localhost', '5001')
 
 const app = express()
 
 app.get('/ipfs/*', async (req, res) => {
-  const path = req.params[0]
-  console.log('Servering', path)
-  const result = await ipfs.api.get(path)
-  res.write(result[0].content)
+  
+  // Get path and default to index.html
+  const pathElements = req.params[0].split('/')
+  if (pathElements[1] === '') pathElements[1] = 'index.html'
+  const path = pathElements.join('/')
+  
+  // Get content
+  // TODO: Use Stream API instead
+  const result = await ipfs.get(path)
+  const content = result[0].content
+
+  // If index.html -> inject script
+  if (pathElements[1] === 'index.html') {
+    console.log('Inject script')
+  }
+  res.write(content)
   res.end()
 })
 
