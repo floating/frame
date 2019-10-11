52ff93595b16e77938a20ceb784fbd14d610d54b
Philip Prophet <philip.prophet@gmail.com>
2019-10-11 16:13:33 +0200

    Working IPFS readable stream PoC

--- main/dapps/server/index.js
+++ main/dapps/server/index.js
@@ -6,16 +6,24 @@ const ipfs = IPFS('localhost', '5001')
 const app = express()
 
 app.get('/ipfs/*', async (req, res) => {
-  
   // Get path and default to index.html
   const pathElements = req.params[0].split('/')
   if (pathElements[1] === '') pathElements[1] = 'index.html'
   const path = pathElements.join('/')
   
   // Get content
-  // TODO: Use Stream API instead
-  const result = await ipfs.get(path)
-  const content = result[0].content
+  // TODO: Handle missing files
+  const stream = ipfs.getReadableStream(path)
+
+  stream.on('data', (file) => {
+    res.writeHead(200)
+    file.content.on('data', (data) => {
+      res.write(data)
+    })
+    file.content.once('end', () => {
+      res.end()
+    })
+  })
 
   // If index.html -> inject script
   if (pathElements[1] === 'index.html') {
