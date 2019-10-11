c6c0f1f0e92bf60e6d76a8ccc1ac2dd20b3d625a
Jordan Muir <cjm712@gmail.com>
2019-10-11 18:28:15 -0400

    Merge IPFS Serve (#267)

--- main/dapps/server/index.js
+++ main/dapps/server/index.js
@@ -5,66 +5,82 @@ const cookie = require('cookie')
 const qs = require('querystring')
 const cheerio = require('cheerio')
 
-const { userData } = require('../../util')
-
+const ipfs = require('../../clients/Ipfs')
 const sessions = require('../sessions')
 
-const asset = require('./asset')
-
-const dapps = path.resolve(userData, 'dapps')
 const inject = fs.readFileSync(path.resolve(__dirname, '../../../bundle/inject.js'), 'utf8')
 
 const storage = {}
-const updateStorage = (res, dapp, session, state) => {
-  if (!dapp || !session || !state) {
+const updateStorage = (res, hash, session, state) => {
+  if (!hash || !session || !state) {
     res.end('Storage update error')
-    console.log('Storage update error: missing dapp, session or state', dapp, session, state)
+    console.log('Storage update error: missing dapp, session or state', hash, session, state)
   } else {
-    const permission = sessions.check(dapp, session)
+    const permission = sessions.check(hash, session)
     if (permission) {
-      storage[dapp] = state
+      storage[hash] = state
       res.writeHead(200)
       res.end()
     } else {
       res.end('Missing session permissions to update storage')
-      console.log('Missing session permissions to update storage', dapp, session, state)
+      console.log('Missing session permissions to update storage', hash, session, state)
     }
   }
 }
 
+const types = {
+  html: 'text/html',
+  css: 'text/css',
+  js: 'application/javascript',
+  ttf: 'application/font-sfnt',
+  svg: 'image/svg+xml',
+  jpeg: 'image/jpeg',
+  jpg: 'image/jpeg',
+  gif: 'image/gif',
+  png: 'image/png'
+}
+
 const handler = (req, res) => {
-  const app = qs.parse(req.url.split('?')[1]).app
-  if (app) {
+  const hash = qs.parse(req.url.split('?')[1]).hash
+  if (hash) {
     const session = qs.parse(req.url.split('?')[1]).session
-    if (!sessions.check(app, session)) return res.end('Missing session permission to access app')
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-    res.setHeader('Set-Cookie', [`_dapp=${app}`, `_session=${session}`])
-    fs.readFile(dapps + '/' + app + '/' + 'index.html', 'utf8', (err, html) => {
-      if (err) return res.end('Error rendering dapp: ' + err.message)
-      const root = cheerio.load(html)
+    if (!sessions.check(hash, session)) return res.end('Missing session permission to access app')
+    ipfs.api.get(`${hash}/index.html`, (err, files) => {
+      if (err) return res.end('Error resolving dapp index: ' + err.message)
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+      res.setHeader('Set-Cookie', [`_hash=${hash}`, `_session=${session}`])
+      const root = cheerio.load(files[0].content.toString('utf8'))
       root('html').prepend(`
         <script>
-          window.__storage__ = ${JSON.stringify(storage[app] || {})}
+          window.__storage__ = ${JSON.stringify(storage[hash] || {})}
           ${inject}
         </script>
-        `)
+      `)
       res.end(root.html())
     })
   } else {
-    const dapp = req.headers.cookie ? cookie.parse(req.headers.cookie)._dapp : null
+    const hash = req.headers.cookie ? cookie.parse(req.headers.cookie)._hash : null
     const session = req.headers.cookie ? cookie.parse(req.headers.cookie)._session : null
-    if (!dapp || !session) return res.end(`Dapp or session missing, dapp: ${dapp}, session: ${session}`)
+    if (!hash || !session) return res.end(`Dapp or session missing, hash: ${hash}, session: ${session}`)
     if (req.method === 'POST') {
       let state = ''
       req.on('data', data => { state += data })
       req.on('end', () => {
-        try { updateStorage(res, dapp, session, JSON.parse(state)) } catch (e) { res.end() }
+        try { updateStorage(res, hash, session, JSON.parse(state)) } catch (e) { res.end() }
       })
     } else {
-      const locator = req.url.split('?')[0].split('/').filter(o => ['', '.', '..'].indexOf(o) === -1)
-      asset(res, dapps + '/' + dapp + '/' + locator.join('/'))
+      const path = `${hash}${req.url.split('?')[0]}`
+      ipfs.api.getReadableStream(path).on('data', file => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+        const ext = path.substr(path.lastIndexOf('.') + 1)
+        res.setHeader('content-type', types[ext] || 'text/plain')
+        file.content.on('data', data => res.write(data))
+        file.content.once('end', () => res.end())
+      })
     }
   }
 }
+
 http.createServer(handler).listen(8421, '127.0.0.1')
