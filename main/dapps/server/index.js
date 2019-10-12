3ed9dce3c14b683ff820d89af52324c76e376cc8
C. Jordan Muir <cjm712@gmail.com>
2019-10-12 16:44:05 -0400

    Comment spacing

--- main/dapps/server/index.js
+++ main/dapps/server/index.js
@@ -13,19 +13,29 @@ const error = (res, code, message) => {
 
 const handler = (req, res) => {
   const { hash, session } = qs.parse(req.url.split('?')[1])
-  if (hash) { // If hash query, this is a request to resolve a dapp and inject it with Frame functionality
+
+  // If hash query, this is a request to resolve a dapp and inject it with Frame functionality
+  if (hash) {
     if (!sessions.verify(hash, session)) return error(res, 403, 'You do not have permissions to access this dapp')
     asset.dapp(res, hash, session)
-  } else { // If no hash query, this is a request from the dapp itself for an asset
+
+  // If no hash query, this is a request from the dapp itself
+  } else {
     const { _hash, _session } = req.headers.cookie ? cookie.parse(req.headers.cookie) : {}
     if (!sessions.verify(_hash, _session)) return error(res, 403, 'You do not have permissions to access this dapp')
-    if (req.method === 'GET') { // GET reqests are for streaming assets
+
+    // GET reqests are for streaming assets
+    if (req.method === 'GET') {
       asset.stream(res, `${_hash}${req.url.split('?')[0]}`)
-    } else if (req.method === 'POST') { // POST requests are updates to storage
+
+    // POST requests are updates to storage
+    } else if (req.method === 'POST') {
       let state = ''
       req.on('data', data => { state += data })
       req.on('end', () => storage.update(res, _hash, state))
-    } else { // Request method not handled by the server
+
+    // Request not handled by the server
+    } else {
       error(res, 404, 'No handler found for request')
     }
   }
