655e6265ec4e7d6f0ef641825042701483bdff01
C. Jordan Muir <cjm712@gmail.com>
2019-12-09 17:33:38 -0500

    Rad migration

---
+++ main/contracts/contractMetadata/contracts.js
@@ -0,0 +1,7 @@
+module.exports = async function (contract, to, web3) {
+  if (contract == 'openzeppelin-contracts/ERC20Detailed.json') {
+    return require('.openzeppelin-contracts/ERC20Detailed.json')
+  } else if (contract == 'aragon-proxy') {
+    return await require('./aragonProxy')(to, web3)
+  }
+}
