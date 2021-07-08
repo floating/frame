c008dd4ff3989adb09259302082220ed3c46b955
Jordan Muir <jordan@frame.sh>
2021-07-08 11:09:43 -0400

    Retag accounts, copy tx send to address, fix typo

--- app/App/Panel/Main/Account/Settings/index.js
+++ app/App/Panel/Main/Account/Settings/index.js
@@ -2,43 +2,74 @@ import React from 'react'
 import Restore from 'react-restore'
 import link from '../../../../../../resources/link'
 
-class Balances extends React.Component {
+class Settings extends React.Component {
   constructor (...args) {
     super(...args)
     this.moduleRef = React.createRef()
-    this.resizeObserver = new ResizeObserver(() => {
-      if (this.moduleRef && this.moduleRef.current) {
-        link.send('tray:action', 'updateAccountModule', this.props.moduleId, { height: this.moduleRef.current.clientHeight })
-      }
-    })
+    if (!this.props.expanded) {
+      this.resizeObserver = new ResizeObserver(() => {
+        if (this.moduleRef && this.moduleRef.current) {
+          link.send('tray:action', 'updateAccountModule', this.props.moduleId, { height: this.moduleRef.current.clientHeight })
+        }
+      })
+    }
     this.state = {
-      expand: false
+      expand: false,
+      name: ''
     }
   }
+
   componentDidMount () {
-    this.resizeObserver.observe(this.moduleRef.current)
-  } 
+    if (this.resizeObserver) this.resizeObserver.observe(this.moduleRef.current)
+    this.nameObs = this.store.observer(() => {
+      this.setState({ name: this.store('main.accounts', this.props.id, 'name') })
+    })
+  }
+
+  componentWillUnmount () {
+    if (this.resizeObserver) this.resizeObserver.disconnect()
+    this.nameObs.remove()
+  }
+
   render () {
     const account = this.store('main.accounts', this.props.id)
     return (
       <div ref={this.moduleRef}>
-        <div className='moduleHeader'>{'Settings'}</div>  
-        <div className='moduleMain moduleMainSettings'> 
-          <div>{account.name}</div> 
-          <div>Status: {account.status}</div>
-          <div>Last Signer: {account.lastSignerType}</div>
-          <div>ENS Name: {account.ensName  ? account.ensName : 'none'}</div>
-          <div>Signer Connected: {account.signer ? 'yes' : 'no'}</div>
-          <div>Account Added: {account.created}</div>
-          <div className='moduleButton moduleButtonBad' onMouseDown={() => {
-            link.rpc('removeAccount', this.props.id, {}, () => {
-              // console.log('Removed account ', address)
-            })
-          }}>Remove This Account</div>
+        {this.props.expanded ? (
+          <div className='moduleHeader'>{'Settings'}</div>  
+        ) : null}
+        <div className='moduleMain moduleMainSettings'>
+          {!this.props.expanded ? (
+            <div className='moduleButton' onMouseDown={() => this.props.expandModule(this.props.moduleId)}>
+              Account Settings
+            </div>
+          ) : (
+            <>
+              <div className='moduleRow'>
+                Account Tag: 
+                <input 
+                  type='text'
+                  tabIndex='-1'
+                  value={this.state.name} 
+                  onChange={(e) => link.send('tray:renameAccount', this.props.id, e.target.value)} 
+                />
+              </div> 
+              <div className='moduleRow'>Status: {account.status}</div>
+              <div className='moduleRow'>ENS Name: {account.ensName  ? account.ensName : 'none'}</div>
+              <div className='moduleRow'>Last Signer Type: {account.lastSignerType}</div>
+              <div className='moduleRow'>Signer Connected: {account.signer ? 'yes' : 'no'}</div>
+              {/* <div className='moduleRow'>Account Added: {account.created}</div> */}
+              <div className='moduleButton moduleButtonBad' onMouseDown={() => {
+                link.rpc('removeAccount', this.props.id, {}, () => {
+                  // console.log('Removed account ', address)
+                })
+              }}>Remove This Account</div>
+            </>
+          )}
         </div>
       </div>
     )
   }
 }
 
-export default Restore.connect(Balances)
+export default Restore.connect(Settings)
