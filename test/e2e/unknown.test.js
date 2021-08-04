1b5b51d331971cc7294dd759d53cbec068654156
Matt Holtzman <matt.holtzman@gmail.com>
2021-08-04 12:58:49 -0400

    add account copy

--- test/e2e/unknown.test.js
+++ test/e2e/unknown.test.js
@@ -1,11 +1,10 @@
 import spectron from 'spectron'
 import electronPath from 'electron'
+import { MouseEvent } from 'globalthis/implementation'
 
-const Application = spectron.Application
-
-const frame = new Application({
+const frame = new spectron.Application({
   path: electronPath,
-  args: ['./compiled/index.js'],
+  args: ['./compiled'],
   // TODO: point this to the container's config directory so beta warning window isn't displayed
   chromeDriverArgs: ['user-data-dir=/Users/matthewholtzman/Library/Application Support/Electron']
 })
@@ -18,10 +17,27 @@ beforeAll(async () => {
   return frame.start().then(app => app.client.waitUntilWindowLoaded())
 }, 30 * 1000)
 
-afterAll(async () => frame && frame.isRunning() ? frame.stop() : true)
+afterAll(async () => {
+  return new Promise(resolve => {
+    if (!frame || !frame.isRunning) return resolve()
+    setTimeout(() => resolve(frame.stop()), 100)
+  })
+})
 
 it('shows some windows', async () => {
   const windowCount = await frame.client.getWindowCount()
 
   expect(windowCount).toBe(2)
+})
+
+it('copies an account address', async () => {
+  const addressDisplay = await frame.client.$('.transactionToAddressLargeWrap')
+  addressDisplay.moveTo({ xOffset: 5, yOffset: 5 })
+
+  const fullAddress = await frame.client.$('.transactionToAddressFull')
+  fullAddress.click({ button: 0 })
+
+  const copiedAddress = await frame.electron.clipboard.readText()
+
+  expect(copiedAddress).toBe('0xe853c56864a2ebe4576a807d26fdc4a0ada51919')
 })
