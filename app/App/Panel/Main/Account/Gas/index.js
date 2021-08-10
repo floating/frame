3c70f663f8f469f88e395969e23de41fa7564ea1
Jordan Muir <jordan@frame.sh>
2021-08-10 17:51:53 -0400

    Add less than range to rounded gwei

--- app/App/Panel/Main/Account/Gas/index.js
+++ app/App/Panel/Main/Account/Gas/index.js
@@ -29,10 +29,19 @@ class Gas extends React.Component {
     return <><span className='timeUnit'>~</span>{Math.round(time / 3600)}<span className='timeUnit'>h</span></>
   }
   toDisplayUSD (bn) {
-    return parseFloat(bn.toNumber() >= 100 ? bn.toFixed(0, BigNumber.ROUND_UP).toString() : bn.toFixed(1, BigNumber.ROUND_UP).toString())
+    return parseFloat(
+      bn.toNumber() >= 1 ? bn.toFixed(0, BigNumber.ROUND_UP).toString() :
+      bn.toFixed(2, BigNumber.ROUND_UP).toString()     
+    )
   }
   roundGwei (gwei) {
-    return parseFloat(Math.ceil(gwei * 10)) / 10
+    if (gwei && gwei < 0.001) return '‹0.001'
+    return parseFloat(
+      gwei >= 10 ? Math.round(gwei) :
+      gwei >= 5 ? Math.round(gwei * 10) / 10 :
+      gwei >= 1 ? Math.round(gwei * 100) / 100 :
+      Math.round(gwei * 1000) / 1000
+    )
   }
   levelDisplay (level) {
     const gwei = weiToGwei(hexToInt(level)) 
@@ -46,42 +55,38 @@ class Gas extends React.Component {
   }
   render () {
     const { type, id } = this.store('main.currentNetwork')
-    const levels = this.store('main.networksMeta', type, id, 'gas.price.levels') || {}
-    const fees = this.store('main.networksMeta', type, id, 'gas.price.fees') || {}
+    const levels = this.store('main.networksMeta', type, id, 'gas.price.levels')
+    const fees = this.store('main.networksMeta', type, id, 'gas.price.fees')
+    const gasPrice = this.levelDisplay(levels.fast)
 
-    const { maxBaseFeePerGas, maxPriorityFeePerGas } = fees
+    const { maxBaseFeePerGas, maxPriorityFeePerGas } = (fees || {})
 
-    const recommendedBaseFee = this.levelDisplay(this.addBufferBaseFee(maxBaseFeePerGas))
     const priorityFee = this.levelDisplay(maxPriorityFeePerGas)
-    const maxFee = parseFloat(Math.round((recommendedBaseFee + priorityFee) * 10) / 10)
+    const maxFee = this.levelDisplay(gasPrice)
 
-    const actualBaseFee = this.roundGwei(((recommendedBaseFee / 21 * 20) / 9) * 8 / 9 * 8)
-    // const likelyBaseFee = this.roundGwei(((recommendedBaseFee / 21 * 20) / 9) * 8)
+    const actualBaseFee = this.roundGwei(((weiToGwei(hexToInt(maxBaseFeePerGas)) / 21 * 20) / 9) * 8 / 9 * 8)
 
-    const lowFee = parseFloat(Math.round((actualBaseFee + priorityFee) * 10) / 10)
-    const highFee = parseFloat(Math.round((recommendedBaseFee + priorityFee) * 10) / 10)
-
+    const lowFee = this.roundGwei(actualBaseFee + priorityFee)
     const layer = this.store('main.networks', type, id, 'layer')
     const nativeCurrency = this.store('main.networksMeta', type, id, 'nativeCurrency')
     const nativeUSD = BigNumber(nativeCurrency && nativeCurrency.usd && layer !== 'testnet' ? nativeCurrency.usd.price : 0)
 
-    // const lowFeeUSD = this.toDisplayUSD(BigNumber(lowFee * 65 * 1000).shiftedBy(-9).multipliedBy(nativeUSD))
-    // const highFeeUSD = this.toDisplayUSD(BigNumber(highFee * 65 * 1000).shiftedBy(-9).multipliedBy(nativeUSD))
-
+    const low = fees ? lowFee : gasPrice
+    const high = fees ? maxFee : gasPrice
     const feeEstimatesUSD = [
       {
-        low: this.txEstimate(lowFee, 21 * 1000, nativeUSD),
-        high: this.txEstimate(highFee, 21 * 1000, nativeUSD),
+        low: this.txEstimate(low, 21 * 1000, nativeUSD),
+        high: this.txEstimate(high, 21 * 1000, nativeUSD),
         label: 'Send ETH'
       }, 
       {
-        low: this.txEstimate(lowFee, 65 * 1000, nativeUSD),
-        high: this.txEstimate(highFee, 65 * 1000, nativeUSD),
+        low: this.txEstimate(low, 65 * 1000, nativeUSD),
+        high: this.txEstimate(high, 65 * 1000, nativeUSD),
         label: 'Send Tokens'
       },
       {
-        low: this.txEstimate(lowFee, 200 * 1000, nativeUSD),
-        high: this.txEstimate(highFee, 200 * 1000, nativeUSD),
+        low: this.txEstimate(low, 200 * 1000, nativeUSD),
+        high: this.txEstimate(high, 200 * 1000, nativeUSD),
         label: 'Dex Swap'
       }
     ]
@@ -94,7 +99,7 @@ class Gas extends React.Component {
         <div className='gasBlock'>
           {this.state.baseHover ? <div className='feeToolTip feeToolTipBase cardShow'>The current base fee is added with a buffer to cover the next 3 blocks, any amount greater than your block's base fee is refunded</div> : null}
           {this.state.prioHover ? <div className='feeToolTip feeToolTipPriority cardShow'>A priority tip paid to validators is added to incentivize quick inclusion of your transaction into a block</div> : null }
-          <div className='gasItem gasItemSmall'>
+          <div className='gasItem gasItemSmall' style={ !fees ? { pointerEvents: 'none', opacity: 0 } : {}}>
             <span className='gasGweiNum'>{actualBaseFee}</span>
             <span className='gasGweiLabel'>{'GWEI'}</span>
             <span className='gasLevelLabel'>{'Current Base'}</span>
@@ -103,25 +108,25 @@ class Gas extends React.Component {
             <div 
               className='gasArrow' 
               onClick={() => this.setState({ baseHover: true })}
-              // onMouseMove={() => this.setState({ baseHover: true })}
+              style={ !fees ? { pointerEvents: 'none', opacity: 0 } : {}}
               onMouseLeave={() => this.setState({ baseHover: false })}
             >
               <div className='gasArrowNotify'>+</div>
               <div className='gasArrowInner'>{svg.chevron(27)}</div>
             </div>
-            <span className='gasGweiNum'>{maxFee}</span>
+            <span className='gasGweiNum'>{gasPrice}</span>
             <span className='gasGweiLabel'>{'GWEI'}</span>
             <span className='gasLevelLabel'>{'Recommended'}</span>
             <div 
               className='gasArrow gasArrowRight'
+              style={ !fees ? { pointerEvents: 'none', opacity: 0 } : {}}
               onClick={() => this.setState({ prioHover: true })}
-              // onMouseMove={() => this.setState({ prioHover: true })}
               onMouseLeave={() => this.setState({ prioHover: false })}
             >
               <div className='gasArrowInner'>{svg.chevron(27)}</div>
             </div>
           </div>
-          <div className='gasItem gasItemSmall'>
+          <div className='gasItem gasItemSmall' style={ !fees ? { pointerEvents: 'none', opacity: 0 } : {}}>
             <span className='gasGweiNum'>{priorityFee}</span>
             <span className='gasGweiLabel'>{'GWEI'}</span>
             <span className='gasLevelLabel'>{'Priority Tip'}</span>
@@ -132,8 +137,8 @@ class Gas extends React.Component {
             return (
               <div className='gasEstimate'>
                 <div className='gasEstimateRange'>
-                  <span style={{ fontSize: '10px', marginRight: '-2px', marginTop: '-1px' }}>{`$`}</span>
-                  <span className='gasEstimateRangeLow'>{`${esimate.low}`}</span>
+                  <span style={{ fontSize: '11px', marginRight: '-2px', marginTop: '-1px' }}>{!esimate.low || esimate.low >= 1 ? `$` : '<$'}</span>
+                  <span className='gasEstimateRangeLow'>{`${!esimate.low ? 0 : esimate.low < 1 ? 1 : esimate.low}`}</span>
                 </div>
                 <div className='gasEstimateLabel'>{esimate.label}</div>
               </div>
