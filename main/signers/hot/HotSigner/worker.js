436c2a2a90ef3155926bea5484b2f7ba2db491ba
Matt Holtzman <matt.holtzman@gmail.com>
2021-08-24 19:05:05 -0400

    use tx type 1 for eip-2930 transactions

--- main/signers/hot/HotSigner/worker.js
+++ main/signers/hot/HotSigner/worker.js
@@ -50,6 +50,7 @@ class HotSignerWorker {
   signTransaction (key, rawTx, pseudoCallback) {
     sign(rawTx, tx => {
       const signedTx = tx.sign(key)
+
       const serialized = signedTx.serialize().toString('hex')
 
       pseudoCallback(null, addHexPrefix(serialized))
