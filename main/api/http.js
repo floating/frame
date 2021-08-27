5fab3a18a2245b9f30fc34410db397616cd7295b
Jordan Muir <jordan@frame.sh>
2021-08-26 20:09:59 -0400

    Share protected methods across interfaces

--- main/api/http.js
+++ main/api/http.js
@@ -7,6 +7,7 @@ const store = require('../store')
 
 const trusted = require('./trusted')
 const validPayload = require('./validPayload')
+const protectedMethods = require('./protectedMethods')
 
 const logTraffic = process.env.LOG_TRAFFIC
 
@@ -25,19 +26,19 @@ const cleanup = id => {
   })
 }
 
-const protectedMethods = [
-  'eth_coinbase',
-  'eth_accounts',
-  'eth_requestAccounts',
-  'eth_sendTransaction',
-  'personal_sign',
-  'personal_ecRecover',
-  'eth_sign',
-  'eth_signTypedData',
-  'eth_signTypedData_v1',
-  'eth_signTypedData_v3',
-  'eth_signTypedData_v4'
-]
+// const protectedMethods = [
+//   'eth_coinbase',
+//   'eth_accounts',
+//   'eth_requestAccounts',
+//   'eth_sendTransaction',
+//   'personal_sign',
+//   'personal_ecRecover',
+//   'eth_sign',
+//   'eth_signTypedData',
+//   'eth_signTypedData_v1',
+//   'eth_signTypedData_v3',
+//   'eth_signTypedData_v4'
+// ]
 
 const handler = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
