54ea5177d2c9db4693f137ac5e6b5b782831e62b
Matt Holtzman <matt.holtzman@gmail.com>
2021-08-27 08:08:16 -0400

    remove comments

--- main/api/http.js
+++ main/api/http.js
@@ -26,20 +26,6 @@ const cleanup = id => {
   })
 }
 
-// const protectedMethods = [
-//   'eth_coinbase',
-//   'eth_accounts',
-//   'eth_requestAccounts',
-//   'eth_sendTransaction',
-//   'personal_sign',
-//   'personal_ecRecover',
-//   'eth_sign',
-//   'eth_signTypedData',
-//   'eth_signTypedData_v1',
-//   'eth_signTypedData_v3',
-//   'eth_signTypedData_v4'
-// ]
-
 const handler = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
