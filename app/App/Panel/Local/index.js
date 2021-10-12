c0b38aec32764aad04077c66bf8f462f661ce397
Matt Holtzman <matt.holtzman@gmail.com>
2021-10-12 16:26:52 -0400

    allow up to 40 Lattice addresses to be loaded

--- app/App/Panel/Local/index.js
+++ app/App/Panel/Local/index.js
@@ -383,7 +383,9 @@ class Settings extends React.Component {
                 onChange={(value) => link.send('tray:action', 'setLatticeAccountLimit', value)}
                 options={[
                   { text: '5', value: 5 },
-                  { text: '10', value: 10 }
+                  { text: '10', value: 10 },
+                  { text: '20', value: 20 },
+                  { text: '40', value: 40 },
                 ]}
               />
             </div>
