0a1316c2d5aff8d2eafe11355b259efa2b8ad666
Matt Holtzman <matt.holtzman@gmail.com>
2021-11-19 14:41:15 -0500

    clean up valid payload

--- main/api/validPayload.ts
+++ main/api/validPayload.ts
@@ -3,24 +3,21 @@ import log from 'electron-log'
 const has = (value: any) => value !== null && value !== undefined
 
 export default function (data: string) {
-  let payload
-
   try {
-    payload = JSON.parse(data)
+    const payload = JSON.parse(data) || {}
+
+    if (has(payload.id) && has(payload.method)) {
+      if (!payload.params) payload.params = []
+
+      return !!(
+        (typeof payload.id === 'number' || typeof payload.id === 'string') &&
+        (typeof payload.jsonrpc === 'string') &&
+        (typeof payload.method === 'string') &&
+        (Array.isArray(payload.params) || typeof payload.params === 'object')
+      ) && payload
+    }
   } catch (e) {
     log.info('Error parsing payload: ', data, e)
-    return false
-  }
-
-  if (payload && typeof payload === 'object' && has(payload.id) && has(payload.method)) {
-    if (!payload.params) payload.params = []
-
-    return !!(
-      (typeof payload.id === 'number' || typeof payload.id === 'string') &&
-      (typeof payload.jsonrpc === 'string') &&
-      (typeof payload.method === 'string') &&
-      (Array.isArray(payload.params) || typeof payload.params === 'object')
-    ) && payload
   }
 
   return false
