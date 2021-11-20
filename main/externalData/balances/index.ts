9e41a7fb89854047a8cae7cc37cc5fca0532d591
Matt Holtzman <matt.holtzman@gmail.com>
2021-11-20 13:45:32 -0500

    add tests for balances module

--- main/externalData/balances/index.ts
+++ main/externalData/balances/index.ts
@@ -12,12 +12,18 @@ import { providers } from 'ethers'
 
 const provider = ethProvider('frame', { name: 'scanWorker' })
 
-interface FoundBalances {
+export interface FoundBalances {
   [address: string]: TokenBalance
 }
 
 interface TokenBalance extends Token {
   balance: BigNumber
+}
+
+export interface BalanceScanOptions {
+  knownTokens?: Token[],
+  omit?: string[],
+  onlyKnown?: boolean // if true, only scan for known tokens, not all tokens for a given chain
 }
 
 async function getChainId () {
@@ -48,8 +54,15 @@ function balanceCalls (owner: string, tokens: Token[]): Call<BigNumber.Value, To
   }))
 }
 
-function intoPositiveBalances (positiveBalances: FoundBalances, tokenBalance: TokenBalance) {
-  if (!tokenBalance.balance.isZero()) {
+function mergeTokenLists (lowerPriority: Token[], higherPriority: Token[]) {
+  return [
+    ...higherPriority,
+    ...(lowerPriority.filter(token => !higherPriority.find(t => t.address === token.address)))
+  ]
+}
+
+function relevantBalances (knownAddresses: string[], positiveBalances: FoundBalances, tokenBalance: TokenBalance) {
+  if (knownAddresses.includes(tokenBalance.address) || !tokenBalance.balance.isZero()) {
     positiveBalances[tokenBalance.address] = tokenBalance
   }
 
@@ -61,28 +74,24 @@ async function getTokenBalance (token: Token, owner: string, provider: providers
 
   try {
     const balance = await contract.balanceOf(owner)
-    return new BigNumber(balance.toHexString()).shiftedBy(-token.decimals)
+    return new BigNumber(balance.toString()).shiftedBy(-token.decimals)
   } catch (e) {
     log.warn(`could not load balance for token with address ${token.address}`, e)
     return new BigNumber(0)
   }
 }
 
-async function getTokenBalancesFromContracts (tokens: Token[], owner: string) {
+async function getTokenBalancesFromContracts (owner: string, tokens: Token[]) {
   const web3Provider = new providers.Web3Provider(provider)
-  const balances = tokens.map(async token => {
-    const balance = await getTokenBalance(token, owner, web3Provider)
+  const balances = tokens.map(async token => ({
+    ...token,
+    balance: await getTokenBalance(token, owner, web3Provider)
+  }))
 
-    return {
-      ...token,
-      balance
-    }
-  })
-
-  return (await Promise.all(balances)).reduce(intoPositiveBalances, {} as FoundBalances)
+  return (await Promise.all(balances))
 }
 
-async function getTokenBalancesFromMulticall (chainId: number, tokens: Token[], owner: string) {
+async function getTokenBalancesFromMulticall (owner: string, tokens: Token[], chainId: number) {
   const calls = balanceCalls(owner, tokens)
   const BATCH_SIZE = 2000
 
@@ -106,28 +115,34 @@ async function getTokenBalancesFromMulticall (chainId: number, tokens: Token[], 
   const fetchResults = await Promise.all(fetches)
   const balanceResults = ([] as TokenBalance[]).concat(...fetchResults)
 
-  return balanceResults.reduce(intoPositiveBalances, {} as FoundBalances)
+  return balanceResults
 }
 
-async function getTokenBalances (owner: string, knownTokens: Token[] = [], omit: string[] = []) {
+async function getTokenBalances (owner: string, opts: BalanceScanOptions = {}) {
   const chainId = await getChainId()
-  const symbolsToOmit = omit.map(symbol => symbol.toLowerCase())
+  const symbolsToOmit = (opts.omit || []).map(symbol => symbol.toLowerCase())
+  const knownTokens = opts.knownTokens || []
   const useMulticall = multicallSupportsChain(chainId)
 
   // for chains that support multicall, we can attempt to load every token that the chain supports,
   // for all other chains we need to call each contract individually so limit the calls
   // to only currently known tokens
-  const tokenList = useMulticall
-    ? tokenLoader.getTokens(chainId) // TODO: merge with known tokens
-    : knownTokens.filter(token => token.chainId === chainId)
+  const tokenList = !opts.onlyKnown && useMulticall
+    ? mergeTokenLists(tokenLoader.getTokens(chainId), knownTokens)
+    : knownTokens
 
   const tokens = tokenList
-    .filter(token => !symbolsToOmit.includes(token.symbol.toLowerCase()))
+    .filter(token => 
+      token.chainId === chainId && !symbolsToOmit.includes(token.symbol.toLowerCase())
+    )
     .map(token => ({ ...token, address: token.address.toLowerCase() }))
   
-  const balances = useMulticall
-    ? await getTokenBalancesFromMulticall(chainId, tokens, owner)
-    : await getTokenBalancesFromContracts(tokens, owner)
+  const allBalances = useMulticall
+    ? await getTokenBalancesFromMulticall(owner, tokens, chainId)
+    : await getTokenBalancesFromContracts(owner, tokens)
+
+  const intoRelevantBalances = relevantBalances.bind(null, knownTokens.map(kt => kt.address.toLowerCase()))
+  const balances = allBalances.reduce(intoRelevantBalances, {} as FoundBalances)
 
   return { chainId, balances }
 }
