379fd4cb6f584707c875f6ce96912385c39d7b90
Jordan Muir <jordan@frame.sh>
2021-12-06 11:04:50 -0500

    Update build for mac, relabel gas settings (#661)
    * Build for x64 and arm64 on mac
    * Priority fee copy

--- app/App/Panel/Main/Account/Requests/TransactionRequest/TxOverlay/TxFeeOverlay/index.js
+++ app/App/Panel/Main/Account/Requests/TransactionRequest/TxOverlay/TxFeeOverlay/index.js
@@ -232,7 +232,7 @@ class TxFeeOverlay extends React.Component {
             }}
           />
         </div>
-        <div className='txFeeOverlayLabel'>Max Base Fee (GWEI)</div>
+        <div className='txFeeOverlayLabel'>Base Fee (GWEI)</div>
       </div>
     )
   }
@@ -269,7 +269,7 @@ class TxFeeOverlay extends React.Component {
             }}
           />
         </div>
-        <div className='txFeeOverlayLabel'>Priority Fee (GWEI) </div>
+        <div className='txFeeOverlayLabel'>Max Priority Fee (GWEI) </div>
       </div>
     )
   }
