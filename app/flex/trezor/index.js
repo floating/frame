15803c6672d325692bef84632b259e887c6071c4
Matt Holtzman <matt.holtzman@gmail.com>
2022-02-11 08:55:44 -0500

    allow passphrase entry on Trezor (#731)
    * allow passphrase entry on Trezor
    
    * use constants

--- app/flex/trezor/index.js
+++ app/flex/trezor/index.js
@@ -1,6 +1,12 @@
-const TrezorConnect = require('trezor-connect').default
-const { DEVICE_EVENT, DEVICE } = require('trezor-connect')
 const EventEmitter = require('events')
+const {
+  default: TrezorConnect,
+  DEVICE_EVENT,
+  DEVICE,
+  UI_EVENT,
+  UI
+} = require('trezor-connect')
+
 const events = new EventEmitter()
 events.setMaxListeners(128)
 let ready = false
@@ -39,13 +45,22 @@ class Device {
     this.emit('trezor:needPhrase', this.device)
   }
 
+  enteringPhrase () {
+    this.emit('trezor:enteringPhrase', this.device)
+
+    TrezorConnect.uiResponse({
+      type: UI.RECEIVE_PASSPHRASE,
+      payload: { value: '', passphraseOnDevice: true }
+    })
+  }
+
   inputPin (pin, cb) {
-    TrezorConnect.uiResponse({ device: this.device, type: 'ui-receive_pin', payload: pin })
+    TrezorConnect.uiResponse({ device: this.device, type: UI.RECEIVE_PIN, payload: pin })
     cb()
   }
 
   inputPhrase (phrase, cb) {
-    TrezorConnect.uiResponse({ device: this.device, type: 'ui-receive_passphrase', payload: { value: phrase } })
+    TrezorConnect.uiResponse({ device: this.device, type: UI.RECEIVE_PASSPHRASE, payload: { value: phrase } })
     cb()
   }
 
@@ -103,6 +118,7 @@ class Trezor {
   constructor (emit) {
     this.emit = emit
     this.devices = {}
+
     TrezorConnect.on(DEVICE_EVENT, e => {
       if (e.type === DEVICE.CONNECT || e.type === DEVICE.CHANGED) {
         // when plugging in the Trezor, the first event can sometimes be "unacquired" which
@@ -120,17 +136,29 @@ class Trezor {
         delete this.devices[e.payload.path]
       }
     })
-    TrezorConnect.on('UI_EVENT', e => {
-      if (e.type === 'ui-request_pin') {
+
+    TrezorConnect.on(UI_EVENT, e => {
+      if (e.type === UI.REQUEST_PIN) {
         const device = this.devices[e.payload.device.path]
         if (device) device.needPin()
-      } else if (e.type === 'ui-request_passphrase') {
+      } else if (e.type === UI.REQUEST_PASSPHRASE) {
         const device = this.devices[e.payload.device.path]
-        if (device) device.needPhrase()
+
+        if (device) {
+          const capabilities = (device.device.features || {}).capabilities || []
+
+          if (capabilities.includes('Capability_PassphraseEntry')) {
+            device.enteringPhrase()
+          } else {
+            device.needPhrase()
+          }
+        }
       }
     })
+
     const manifest = { email: 'jordan@frame.sh', appUrl: 'https://frame.sh' }
     const config = { manifest, popup: false, webusb: false, debug: false, lazyLoad: false }
+
     try {
       TrezorConnect.init(config).then(() => {
         ready = true
