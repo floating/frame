3ef9ef545ef997b89f066b8fa76ddb262c5d8328
goosewobbler <goosewobbler@pm.me>
2022-02-14 19:40:02 +0000

    don't create servers on load

--- main/api/index.js
+++ main/api/index.js
@@ -1,10 +1,19 @@
 const { http, https } = require('./http')
 const ws = require('./ws')
+let wsHttp
+let wsHttps
 
-ws(http()).listen(1248, '127.0.0.1')
+module.exports = {
+  init: () => {
+    if (wsHttp === undefined) {
+      wsHttp = ws(http()).listen(1248, '127.0.0.1')
+    }
 
-// this needs to be re-initialised when a valid cert is created / selected
-const httpsServer = https()
-if (httpsServer) {
-  ws(https()).listen(1249, '127.0.0.1')
+    if (wsHttps === undefined) {
+      const httpsServer = https()
+      if (httpsServer) {
+        wsHttps = ws(https()).listen(1249, '127.0.0.1')
+      }
+    }
+  }
 }
