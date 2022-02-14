ed4f3917e8c10d0b62b9020012442e443af3b741
Matt Holtzman <matt.holtzman@gmail.com>
2022-02-14 18:07:57 -0500

    update notarization process

--- build/notarize.js
+++ build/notarize.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const { execFileSync } = require('child_process')
 const path = require('path')
 const electronNotarize = require('electron-notarize')
 
@@ -7,7 +8,9 @@ module.exports = async function (params) {
   const appId = 'sh.frame.app' // Same appId in electron-builder
   const appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`)
   if (!fs.existsSync(appPath)) throw new Error(`Cannot find application at: ${appPath}`)
+
   console.log(`Notarizing ${appId} found at ${appPath}`)
+
   try {
     await electronNotarize.notarize({
       appBundleId: appId,
@@ -15,8 +18,21 @@ module.exports = async function (params) {
       appleId: process.env.APPLE_ID,
       appleIdPassword: process.env.APPLE_ID_PASSWORD
     })
+
+    // verify signed and notarized application
+    execFileSync('spctl', [
+      '--assess',
+      '--type',
+      'execute',
+      '--verbose',
+      '--ignore-cache',
+      '--no-cache',
+      appPath
+    ], {})
+
+    console.log(`Successfully notarized ${appId}`)
   } catch (error) {
     console.error(error)
+    throw error
   }
-  console.log(`Done notarizing ${appId}`)
 }
