98f3963ff50fb428c21b0507d012099d09888ff6
Jordan Muir <jordan@frame.sh>
2022-04-05 09:07:47 -0400

    Add ext matching in production

--- main/api/isFrameExtension.js
+++ main/api/isFrameExtension.js
@@ -1,9 +1,32 @@
 const queryString = require('query-string')
 
+const dev = process.env.NODE_ENV === 'development'
+
 module.exports = req => {
   const origin = req.headers.origin
+  console.log('isFrameExtension?', origin)
   if (!origin) return false
   const query = queryString.parse(req.url.replace('/', ''))
-  if (origin.indexOf('chrome-extension://') > -1 || origin.indexOf('moz-extension://') > -1) return query.identity === 'frame-extension'
+
+  // Match production extension ids exactly
+  if (
+    origin === 'chrome-extension://ldcoohedfbjoobcadoglnnmmfbdlmmhf' || // production chrome
+    origin === 'moz-extension://bd0560a2-8c13-4cb4-a856-eceab7e771d8', // production firefox
+    origin === 'safari-web-extension://7655eadc-658f-4faf-b22c-bff822d0154e' // production safari
+  ) {
+    return true
+  }
+
+  // If in dev mode, match any extension where query.identity === 'frame-extension'
+  if (dev) {
+    if (
+      origin.startsWith('chrome-extension://') > -1 || 
+      origin.startsWith('moz-extension://') > -1 ||
+      origin.startsWith('safari-web-extension://')
+    ) {
+      return query.identity === 'frame-extension'
+    }
+  }
+
   return false
 }
