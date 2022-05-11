03f42009ce48f2465352160064a7953b6c31b8ec
goosewobbler <goosewobbler@pm.me>
2022-05-11 19:13:39 +0100

    add origins tests

---
+++ test/e2e/origins.test.js
@@ -0,0 +1,29 @@
+
+import provider from 'eth-provider'
+import store from '../../main/store'
+
+jest.mock('../../main/store/persist')
+
+const frame = provider('frame', { origin: 'frame.sh' })
+
+it('should be able to change the chain', async done => {
+  try {
+    store.observer(() => {
+      const chainId = store('main.dapps')
+      console.log('got chain yo', chainId)
+      if (chainId === '0x4') {
+        done()
+      }
+    })
+    console.log('requesting chain change')
+    await frame.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: '0x4' }],
+      origin: 'https://frame.sh'
+    })
+    console.log('change requested')
+    // verify chain changed only for that origin
+  } catch (err) {
+    console.log(err)
+  }
+}, 30 * 1000)
