9e58303a8fd2dac4dec35ab8a5c223fdc9fe13c3
Matt Holtzman <matt.holtzman@gmail.com>
2022-05-23 09:24:40 -0500

    fix typing

--- main/api/origin.ts
+++ main/api/origin.ts
@@ -2,35 +2,39 @@ import { v5 as uuidv5 } from 'uuid'
 import log from 'electron-log'
 import store from '../store'
 
-export function updateOrigin (payload: RPCRequestPayload, originName: string) {
-    if (!originName) {
-      log.warn(`Received payload with no origin: ${payload.method}`)
-  
-      //log.warn(`Received payload with no origin: ${JSON.stringify(payload)}`)
-      return { ...payload, chainId: payload.chainId || '0x1' }
-    }
-    
-    const originId = uuidv5(originName, uuidv5.DNS)
-    const existingOrigin = store('main.origins', originId)
-  
-    // @ts-ignore
-    if (!existingOrigin && !payload.__extensionConnecting) {
-      // the extension will attempt to send messages (eth_chainId and net_version) in order
-      // to connect. we don't want to store these origins as they'll come from every site
-      // the user visits in their browser
-      store.initOrigin(originId, {
-        name: originName,
-        chain: {
-          id: 1,
-          type: 'ethereum'
-        }
-      })
-    }
-    
-    return {
-      ...payload,
-      chainId: payload.chainId || `0x${(existingOrigin?.chain.id || 1).toString(16)}`,
-      _origin: originId
-    }
+interface ExtensionPayload extends JSONRPCRequestPayload {
+  chainId?: string,
+  __frameOrigin?: string,
+  __extensionConnecting?: boolean
+}
+
+export function updateOrigin (payload: ExtensionPayload, originName: string) {
+  if (!originName) {
+    log.warn(`Received payload with no origin: ${payload.method}`)
+
+    //log.warn(`Received payload with no origin: ${JSON.stringify(payload)}`)
+    return { ...payload, chainId: payload.chainId || '0x1' }
   }
-  
+
+  const originId = uuidv5(originName, uuidv5.DNS)
+  const existingOrigin = store('main.origins', originId)
+
+  if (!existingOrigin && !payload.__extensionConnecting) {
+    // the extension will attempt to send messages (eth_chainId and net_version) in order
+    // to connect. we don't want to store these origins as they'll come from every site
+    // the user visits in their browser
+    store.initOrigin(originId, {
+      name: originName,
+      chain: {
+        id: 1,
+        type: 'ethereum'
+      }
+    })
+  }
+
+  return {
+    ...payload,
+    chainId: payload.chainId || `0x${(existingOrigin?.chain.id || 1).toString(16)}`,
+    _origin: originId
+  }
+}
