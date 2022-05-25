3100016d08b0da93dc7f7bd189a6748fd7bc1250
Matt Holtzman <matt.holtzman@gmail.com>
2022-05-25 10:55:49 -0500

    use correct path

--- app/App/Main/Account/Balances/index.js
+++ app/App/Main/Account/Balances/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 import Restore from 'react-restore'
 import link from '../../../../../resources/link'
 import svg from '../../../../../resources/svg'
-import { isNetworkConnected } from '../../../resources/utils/chains'
+import { isNetworkConnected } from '../../../../../resources/utils/chains'
 
 import chainMeta from '../../../../../resources/chainMeta'
 
