5723d61e7373c04dc9c99734e495780ddef6e079
goosewobbler <goosewobbler@pm.me>
2022-07-04 18:40:46 +0100

    inline beforeSend return

--- main/sentry/index.ts
+++ main/sentry/index.ts
@@ -31,14 +31,12 @@ export function init () {
     // only use IPC from renderer process, not HTTP
     ipcMode: Sentry.IPCMode.Classic,
     dsn: 'https://7b09a85b26924609bef5882387e2c4dc@o1204372.ingest.sentry.io/6331069',
-    beforeSend: (evt: Event) => {
-      return {
-        ...evt,
-        exception: { values: getSentryExceptions(evt) },
-        user: { ...evt.user, ip_address: undefined }, // remove IP address
-        tags: { ...evt.tags, 'frame.instance_id': store('main.instanceId') },
-        extra: getCrashReportFields()
-      }
-    }
+    beforeSend: (evt: Event) => ({
+      ...evt,
+      exception: { values: getSentryExceptions(evt) },
+      user: { ...evt.user, ip_address: undefined }, // remove IP address
+      tags: { ...evt.tags, 'frame.instance_id': store('main.instanceId') },
+      extra: getCrashReportFields()
+    })
   })
 }
