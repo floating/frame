460c24c853c6a9eb5ef23289cfee78c46905a3b5
Matt Holtzman <matt.holtzman@gmail.com>
2022-07-08 14:09:16 +0200

    Worker process (#934)
    * add infrastructure for worker processes
    
    * add name
    
    * test
    
    * finish tests

--- @types/frame/environment.d.ts
+++ @types/frame/environment.d.ts
@@ -1,10 +1,10 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      BUNDLE_LOCATION: string
-      NODE_ENV: 'test' | 'development' | 'production',
+      BUNDLE_LOCATION?: string
+      NODE_ENV?: 'test' | 'development' | 'production'
       // use this to override the log level in development
-      LOG_LEVEL: 'silly' | 'debug' | 'verbose' | 'info' | 'warn' | 'error'
+      LOG_LEVEL?: 'silly' | 'debug' | 'verbose' | 'info' | 'warn' | 'error'
     }
   }
 }
