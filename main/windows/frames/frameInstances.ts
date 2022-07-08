247e043c2e482ba6c7986e2523a1570b761b31a0
Matt Holtzman <matt.holtzman@gmail.com>
2022-07-08 08:28:39 +0200

    add infrastructure for worker processes

--- main/windows/frames/frameInstances.ts
+++ main/windows/frames/frameInstances.ts
@@ -29,7 +29,7 @@ export default {
     place(frameInstance)
   },
   create: (frame: Frame) => {  
-    const preload = path.resolve(__dirname, process.env.BUNDLE_LOCATION, 'bridge.js')
+    const preload = path.resolve(__dirname, (process.env.BUNDLE_LOCATION || ''), 'bridge.js')
   
     const frameInstance: FrameInstance = new BrowserWindow({
       x: 0,
