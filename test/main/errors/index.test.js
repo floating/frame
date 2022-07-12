4ea2d8818c0c36141019f7471029007bb482ebdb
Matt Holtzman <matt.holtzman@gmail.com>
2022-07-12 18:56:37 +0200

    add path test, minor cleanup

--- test/main/errors/index.test.js
+++ test/main/errors/index.test.js
@@ -14,7 +14,8 @@ afterAll(() => {
 
 describe('sentry', () => {
   const mockEvent = (event) => Sentry.init.mock.calls[0][0].beforeSend(event)
-  const mockEvents = (events) => events.map(mockEvent)
+  const mockEvents = (numEvents) => [...Array(numEvents).fill({})].map(mockEvent)
+
   const validEvent = {
     exception: {
       values: [],
@@ -34,6 +35,7 @@ describe('sentry', () => {
 
   it('should initialize sentry with the expected object', () => {
     init()
+
     expect(Sentry.init).toHaveBeenCalledWith({
       beforeSend: expect.any(Function),
       dsn: 'https://7b09a85b26924609bef5882387e2c4dc@o1204372.ingest.sentry.io/6331069',
@@ -41,8 +43,9 @@ describe('sentry', () => {
     })
   })
 
-  it('should strip asar paths from stackframe modules', () => {
+  it('should strip asar paths from stackframe modules on Windows', () => {
     init()
+
     const sentryEvent = mockEvent({
       exception: {
         values: [{
@@ -60,7 +63,39 @@ describe('sentry', () => {
           }]
       }
     })
+
     const stackFrameModules = sentryEvent.exception.values[0].stacktrace.frames.map((frame) => frame.module)
+
+    expect(stackFrameModules).toStrictEqual([
+      "{asar}/node_modules/electron-updater/out/AppUpdater",
+      "node:domain",
+      "{asar}/compiled/main/signers/lattice/Lattice/index"
+    ])
+  })
+
+  it('should strip asar paths from stackframe modules on non-Windows OSes', () => {
+    init()
+
+    const sentryEvent = mockEvent({
+      exception: {
+        values: [{
+          "type": "Error",
+          "value": "Cannot find latest.yml in the latest release artifacts (https://github.com/floating/frame/releases/download/v0.5.0-beta.20/latest.yml): HttpError: 404 \n\"method: GET url: https://github.com/floating/frame/releases/download/v0.5.0-beta.20/latest.yml\\n\\...",
+          "stacktrace": {
+              "frames": [{
+                "module": "/home/RickyBobby/.config/frame/resources/app.asar/node_modules/electron-updater/out/AppUpdater",
+              }, {
+                "module": "node:domain",
+              }, {
+                "module": "/home/RickyBobby/.config/frame/resources/app.asar/compiled/main/signers/lattice/Lattice/index",
+              }]
+            },
+          }]
+      }
+    })
+
+    const stackFrameModules = sentryEvent.exception.values[0].stacktrace.frames.map((frame) => frame.module)
+
     expect(stackFrameModules).toStrictEqual([
       "{asar}/node_modules/electron-updater/out/AppUpdater",
       "node:domain",
@@ -70,36 +105,39 @@ describe('sentry', () => {
 
   it('should drop events once the rate limit has been reached', () => {
     init()
-    const sentEvents = mockEvents(Array(10).fill({}))
+
+    const sentEvents = mockEvents(10)
 
     // after the limit is reached, this function will return a falsy value rather than the actual event
     const reportedEvents = sentEvents.filter(evt => !!evt)
     expect(reportedEvents).toStrictEqual(Array(5).fill(validEvent))
   })
 
-  it('should send events after the rate limit recovery period has elapsed', () => {
-    const events = Array(5).fill({})
+  it('should send events after the rate limit recovery period has partially elapsed', () => {
     init()
-    mockEvents(events)
-    jest.advanceTimersByTime(60_000)
-    expect(mockEvents(events)).toStrictEqual(
-      [validEvent, null, null, null, null]
-    )
-    jest.advanceTimersByTime(2 * 60_000)
-    expect(mockEvents(events)).toStrictEqual(
-      [validEvent, validEvent, null, null, null]
-    )
+
+    // send 5 events to exhaust the initial rate limit
+    mockEvents(5)
+
+    // the rate limit backs off by one every hour
     jest.advanceTimersByTime(3 * 60_000)
-    expect(mockEvents(events)).toStrictEqual(
-      [validEvent, validEvent, validEvent, null, null]
-    )
-    jest.advanceTimersByTime(4 * 60_000)
-    expect(mockEvents(events)).toStrictEqual(
-      [validEvent, validEvent, validEvent, validEvent, null]
-    )
-    jest.advanceTimersByTime(100 * 60_000)
-    expect(mockEvents(events)).toStrictEqual(
-      [validEvent, validEvent, validEvent, validEvent, validEvent]
-    )
+
+    const sentEvents = mockEvents(5)
+    const reportedEvents = sentEvents.filter(evt => !!evt)
+    expect(reportedEvents.length).toBe(3)
+  })
+
+  it('should send events after the rate limit recovery period has fully expired', () => {
+    init()
+
+    // send 5 events to exhaust the initial rate limit
+    mockEvents(5)
+
+    // the rate limit backs off by one every hour
+    jest.advanceTimersByTime(10 * 60_000)
+
+    const sentEvents = mockEvents(5)
+    const reportedEvents = sentEvents.filter(evt => !!evt)
+    expect(reportedEvents.length).toBe(5)
   })
 })
