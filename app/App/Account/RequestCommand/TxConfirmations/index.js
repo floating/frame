c75335546ec9da30332ef2764a6cebaec00944ef
Jordan Muir <jordan@frame.sh>
2022-09-08 17:10:57 -0400

    Refactor request approve

--- app/App/Account/RequestCommand/TxConfirmations/index.js
+++ app/App/Account/RequestCommand/TxConfirmations/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Restore from 'react-restore'
-import svg from '../../../../resources/svg'
+import svg from '../../../../../resources/svg'
 
 class TxConfirmations extends React.Component {
   render () {
