1aa16543aba5caf20376bf6c318a36f0bf97fa05
goosewobbler <432005+goosewobbler@users.noreply.github.com>
2022-09-08 18:37:16 +0100

    Fix sentry throw (#1024)
    Co-authored-by: goosewobbler <goosewobbler@pm.me>

--- main/signers/ledger/Ledger/eth.ts
+++ main/signers/ledger/Ledger/eth.ts
@@ -8,6 +8,7 @@ import Eth from '@ledgerhq/hw-app-eth'
 import { Derivation, getDerivationPath, deriveHDAccounts } from '../../Signer/derive'
 import { TransactionData } from '../../../../resources/domain/transaction'
 import { sign } from '../../../transaction'
+import { DeviceError } from '.'
 
 export default class LedgerEthereumApp {
   private eth: Eth;
@@ -57,7 +58,7 @@ export default class LedgerEthereumApp {
       domainSeparatorHex = TypedDataUtils.hashStruct('EIP712Domain', domain, types).toString('hex')
       hashStructMessageHex = TypedDataUtils.hashStruct(primaryType as string, message, types).toString('hex')
     } catch (e) {
-      throw { statusCode: 99901, message: 'Invalid typed data' }
+      throw new DeviceError('Invalid typed data', 99901)
     }
 
     const signature = await this.eth.signEIP712HashedMessage(path, domainSeparatorHex, hashStructMessageHex)
