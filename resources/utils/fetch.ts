cd1db68132b7f510d7097caf0a18868f25fb370f
goosewobbler <goosewobbler@pm.me>
2022-09-14 19:25:53 +0100

    use timeout

--- resources/utils/fetch.ts
+++ resources/utils/fetch.ts
@@ -1,6 +1,8 @@
 import fetch, { RequestInit } from 'node-fetch'
 
-export async function fetchWithTimeout (url: string, options: RequestInit, timeout: number) {  //@ts-ignore
-  const signal = AbortSignal.timeout(timeout)
-  return fetch(url, { ...options, signal })
+export async function fetchWithTimeout (url: string, options: RequestInit, timeout: number) {  
+  const controller = new AbortController() 
+  setTimeout(() => controller.abort(), timeout)
+  // @ts-ignore
+  return fetch(url, { ...options, signal: controller.signal })
 }
