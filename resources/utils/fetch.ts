d815bbd1bad737b64d47b8f5c182ed2019daab9c
goosewobbler <goosewobbler@pm.me>
2022-09-15 19:56:30 +0100

    remove ts-ignore

--- resources/utils/fetch.ts
+++ resources/utils/fetch.ts
@@ -3,6 +3,5 @@ import fetch, { RequestInit } from 'node-fetch'
 export async function fetchWithTimeout (url: string, options: RequestInit, timeout: number) {  
   const controller = new AbortController() 
   setTimeout(() => controller.abort(), timeout)
-  // @ts-ignore
-  return fetch(url, { ...options, signal: controller.signal })
+  return fetch(url, { ...options, signal: controller.signal as RequestInit['signal'] })
 }
