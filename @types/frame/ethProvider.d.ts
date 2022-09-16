8031096d3fae87388a67e0606ac7e1b464bbbc55
goosewobbler <goosewobbler@pm.me>
2022-09-16 23:14:20 +0100

    misc typing

--- @types/frame/ethProvider.d.ts
+++ @types/frame/ethProvider.d.ts
@@ -5,20 +5,48 @@ declare module 'eth-provider' {
   }
   
   interface RequestPayload {
-    id?: number,
-    jsonrpc?: '2.0',
-    method: string,
-    params?: any[],
+    id?: number
+    jsonrpc?: string
+    method: string
+    params?: any[]
     chainId?: string
   }
 
   interface EthereumProvider extends NodeJS.EventEmitter {
-    constructor()
-
+    private readonly connection
+    private readonly eventHandlers
+    private readonly promises
+    private readonly attemptedSubscriptions
+    private subscriptions
+    private networkVersion?
+    private manualChainId?
+    private providerChainId?
+    private checkConnectionRunning
+    private checkConnectionTimer?
+    private nextId
     connected: boolean
-    chainId: string
-    request(payload: RequestPayload)
-    setChain(chainId: string)
+    accounts: string[]
+    selectedAddress: string
+    coinbase: string
+    constructor(connection: Connection)
+    get chainId(): string | undefined
+    checkConnection(retryTimeout?: number): Promise<void>
+    private attemptedSubscription
+    private setSubscriptionAttempted
+    startSubscription(event: string): Promise<void>
+    private resumeSubscriptions
+    enable(): Promise<string[]>
+    private doSend
+    send(methodOrPayload: string | Payload, callbackOrArgs: Callback<Response> | unknown[]): Promise<unknown>
+    private sendBatch
+    subscribe(type: string, method: string, params?: never[]): Promise<string>
+    unsubscribe(type: string, id: string): Promise<true | undefined>
+    sendAsync(rawPayload: Payload | Payload[], cb: Callback<Response> | Callback<Response[]>): Promise<void | Error>
+    private sendAsyncBatch
+    isConnected(): boolean
+    close(): void
+    request<T>(payload: Payload): Promise<T>
+    setChain(chainId: string | number): void
   }
 
   export default function provider (targets?: string | string[], opts?: ProviderOpts): EthereumProvider
