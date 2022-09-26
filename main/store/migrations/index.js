35148ea1c2400ca5557777c340861863ecaf4461
goosewobbler <goosewobbler@pm.me>
2022-09-26 20:40:45 +0100

    update again

--- main/store/migrations/index.js
+++ main/store/migrations/index.js
@@ -447,33 +447,31 @@ const migrations = {
     // we removed support for the following goerli RPCs so reset the connections 
     // to defaults when the user was previously connecting to them
     const removedGoerliRPCs = ['mudit', 'slockit', 'prylabs']
-    const goerliPrimaryConnection = initial.main.networks.ethereum[5].connection.primary.current
-    const goerliSecondaryConnection = initial.main.networks.ethereum[5].connection.secondary.current
+    const goerli = initial.main.networks.ethereum[5]
+    const goerliPrimaryConnection = goerli.connection.primary.current
+    const goerliSecondaryConnection = goerli.connection.secondary.current
     
     if (removedGoerliRPCs.includes(goerliPrimaryConnection)) {
       initial.main.networks.ethereum[5] = {
-        ...initial.main.networks.ethereum[5],
+        ...goerli,
         connection: {
-          ...initial.main.networks.ethereum[5].connection,
+          ...goerli.connection,
           primary: { on: false, current: 'custom', status: 'loading', connected: false, type: '', network: '', custom: '' }
         }
       }
     }
     if (removedGoerliRPCs.includes(goerliSecondaryConnection)) {
       initial.main.networks.ethereum[5] = {
-        ...initial.main.networks.ethereum[5],
+        ...goerli,
         connection: {
-          ...initial.main.networks.ethereum[5].connection,
+          ...goerli.connection,
           secondary: { on: false, current: 'custom', status: 'loading', connected: false, type: '', network: '', custom: '' }
         }
       }
     }
 
     // if neither primary nor secondary is enabled then we switch the overall connection off
-    const enabledConnection = initial.main.networks.ethereum[5].connection.primary.on || initial.main.networks.ethereum[5].connection.secondary.on
-    if (!enabledConnection) {
-      initial.main.networks.ethereum[5].connection.on = false
-    }
+    initial.main.networks.ethereum[5].connection.on = goerli.connection.primary.on || goerli.connection.secondary.on
 
     return initial
   },
