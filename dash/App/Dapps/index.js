172900a50290a93055d8727d9984887aacc3fbba
Jordan Muir <jordan@frame.sh>
2022-09-28 02:48:34 -0400

    Fix chain color and icon in dapps view

--- dash/App/Dapps/index.js
+++ dash/App/Dapps/index.js
@@ -126,16 +126,15 @@ class _OriginModule extends React.Component {
 
 const OriginModule = Restore.connect(_OriginModule)
 
-const ChainOrigins = ({ chain: { id, name }, origins }) => {
-  const chainColor = this.store('main.networksMeta.ethereum', id, 'primaryColor')
+const ChainOrigins = ({ chain: { name }, origins, primaryColor, icon }) => {
   return (
     <>
       <div className='originTitle'>
         <div className='originTitleIcon'>
           <RingIcon 
             small={true}
-            color={`var(--${chainColor})`}
-            svgName={name}
+            color={`var(--${primaryColor})`}
+            img={icon}
           />
         </div>
         <div className='originTitleText'>{name}</div>
@@ -171,10 +170,11 @@ class Dapps extends React.Component {
           {
             enabledChains.map(chain => {
               const chainOrigins = getOriginsForChain(chain, origins)
+              const { primaryColor, icon } = this.store('main.networksMeta.ethereum', chain.id)
   
               return chainOrigins.length === 0
                 ? <></>
-                : <ChainOrigins chain={chain} origins={chainOrigins} />
+                : <ChainOrigins chain={chain} origins={chainOrigins} primaryColor={primaryColor} icon={icon} />
             })
           }
           <div className={'clearOriginsButton'} onClick={clearOriginsClickHandler} >
