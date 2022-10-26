acb0b7e81cd882edba9fc572274ff33ee4242f16
Jamie M <jamie.michel@hotmail.com>
2022-10-26 17:31:41 +0200

    update initial version

--- main/store/state/index.js
+++ main/store/state/index.js
@@ -154,7 +154,7 @@ const initial = {
   },
   platform: process.platform,
   main: {
-    _version: main('_version', 23),
+    _version: main('_version', 26),
     instanceId: main('instanceId', generateUuid()),
     colorway: main('colorway', 'dark'),
     colorwayPrimary: {
