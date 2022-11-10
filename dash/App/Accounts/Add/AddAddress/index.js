e3b2d4544c9f1bb108fe5f69897258ec35a2de50
Jamie M <jamie.michel@hotmail.com>
2022-11-10 22:50:06 +0100

    using navBack with steps

--- dash/App/Accounts/Add/AddAddress/index.js
+++ dash/App/Accounts/Add/AddAddress/index.js
@@ -136,7 +136,7 @@ class AddAddress extends React.Component {
                   :
                   <>
                    <div className='addAccountItemOptionTitle'>{'account added successfully'}</div>
-                   <div className='addAccountItemOptionSubmit' onClick={() => link.send('tray:action', 'navDash', { view: 'accounts', data: {} })}>back</div>
+                   <div className='addAccountItemOptionSubmit' onClick={() => link.send('tray:action', 'navBack', 'dash', 2)}>back</div>
                   </>}
                 </div>
               </div>
