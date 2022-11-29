aee9f78a6295b72a29ad96b4276f9fdf0fe50a81
goosewobbler <goosewobbler@pm.me>
2022-11-29 19:30:03 +0000

    address review comments

--- resources/Components/DisplayValue/index.js
+++ resources/Components/DisplayValue/index.js
@@ -1,48 +1,38 @@
 import React from 'react'
 import { displayValueData } from '../../utils/displayValue'
 
-const Approx = ({ approximationSymbol }) => {
-  return approximationSymbol && (
-    <span className='displayValueApprox'>
-      {approximationSymbol}
-    </span>
-  )
-}
+const ApproximateValue = ({ approximationSymbol }) => (
+  <span className='displayValueApprox'>
+    {approximationSymbol}
+  </span>
+)
 
-const FiatSymbol = ({ fiatSymbol }) => {
-  return fiatSymbol && (
-    <span className='displayValueFiat'>
-      {fiatSymbol}
-    </span>
-  )
-}
+const FiatSymbol = ({ fiatSymbol }) => (
+  <span className='displayValueFiat'>
+    {fiatSymbol}
+  </span>
+)
 
-const Symbol = ({ currencySymbol, show }) => {
-  return currencySymbol && show && (
-    <span className='displayValueSymbol'>
-      {currencySymbol.toUpperCase()}
-    </span>
-  )
-}
 
-const Main = ({ displayValue }) => {
-  return (
-    <span className='displayValueMain'>
-      {displayValue}
-    </span>
-  )
-}
+const Symbol = ({ currencySymbol }) => (
+  <span className='displayValueSymbol'>
+    {currencySymbol.toUpperCase()}
+  </span>
+)
 
-const Unit = ({ displayUnit }) => {
-  return displayUnit && (
-    <span className='displayValueUnit'>
-      {displayUnit.shortName}
-    </span>
-  )
-}
+const Main = ({ displayValue }) => (
+  <span className='displayValueMain'>
+    {displayValue}
+  </span>
+)
 
-export const DisplayValue = props => {
+const Unit = ({ displayUnit }) => (
+  <span className='displayValueUnit'>
+    {displayUnit.shortName}
+  </span>
+)
 
+export const DisplayValue = (props) => {
   const {
     value, 
     valueData, 
@@ -65,18 +55,18 @@ export const DisplayValue = props => {
     <div className='displayValue' data-testid='display-value'>
       {type === 'fiat' ? (
         <>
-          <Approx approximationSymbol={approximationSymbol} />
-          <FiatSymbol fiatSymbol={currencySymbol} />
+          {approximationSymbol && <ApproximateValue approximationSymbol={approximationSymbol} />}
+          {currencySymbol && <FiatSymbol fiatSymbol={currencySymbol} />}
         </>
       ) : (
         <>
-          <Symbol currencySymbol={currencySymbol} show={currencySymbolPosition === 'first'} />
-          <Approx approximationSymbol={approximationSymbol} />
+          {currencySymbol && currencySymbolPosition === 'first' && <Symbol currencySymbol={currencySymbol} />}
+          {approximationSymbol && <ApproximateValue approximationSymbol={approximationSymbol} />}
         </>
       )}
       <Main displayValue={displayValue} />
-      <Unit displayUnit={displayUnit} />
-      <Symbol currencySymbol={currencySymbol} show={currencySymbolPosition === 'last'} />
+      {displayUnit && <Unit displayUnit={displayUnit} />}
+      {currencySymbol && currencySymbolPosition === 'last' && <Symbol currencySymbol={currencySymbol} />}
     </div>
   )
 }
