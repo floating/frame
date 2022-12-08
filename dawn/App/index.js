63d316c2cb1429c2653882c7bab29d3d1d52ccf8
goosewobbler <goosewobbler@pm.me>
2022-12-08 03:05:23 +0000

    styled-components

--- dawn/App/index.js
+++ dawn/App/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import Restore from 'react-restore'
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+  color: blue;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`
 
 class App extends React.Component {
   constructor(...args) {
@@ -10,7 +20,7 @@ class App extends React.Component {
   render() {
     return (
       <div className='dawn'>
-        <h1>{'A New Dawn'}</h1>
+        <StyledButton>{'A New Dawn'}</StyledButton>
       </div>
     )
   }
