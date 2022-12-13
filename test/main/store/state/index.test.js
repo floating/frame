bbbadf82a0f7ed84648771ab437d2d6fec1c88ba
Matt Holtzman <matt.holtzman@gmail.com>
2022-12-13 18:16:29 -0500

    update test

--- test/main/store/state/index.test.js
+++ test/main/store/state/index.test.js
@@ -1,5 +1,3 @@
-import migrations from '../../../../main/store/migrations'
-
 jest.mock('electron-log', () => ({ info: console.log, error: jest.fn() }))
 jest.mock('electron', () => ({ app: { on: jest.fn(), getPath: jest.fn() } }))
 
@@ -35,11 +33,27 @@ jest.mock('../../../../main/store/persist', () => {
   return { get }
 })
 
+afterEach(() => {
+  // ensure modules are reloaded before each test
+  jest.resetModules()
+})
+
 it('maintains backwards compatible access to the current version of state', async () => {
   // load state already migrated to version 2 and make sure version 1 values are available
+  const { default: migrations } = await import('../../../../main/store/migrations')
   migrations.latest = 1
 
   const { default: state } = await import('../../../../main/store/state')
 
   expect(state().main.instanceId).toBe('test-frame')
+})
+
+it('loads values from the current version of the state', async () => {
+  // load state already migrated to version 2 and make sure version 1 values are available
+  const { default: migrations } = await import('../../../../main/store/migrations')
+  migrations.latest = 2
+
+  const { default: state } = await import('../../../../main/store/state')
+
+  expect(state().main.instanceId).toBe('test-brand-new-frame')
 })
