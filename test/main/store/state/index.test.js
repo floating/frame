c0ebf61beab03070e7d1067cbcde4f535b63d062
Matt Holtzman <matt.holtzman@gmail.com>
2022-12-14 20:54:17 -0500

    add test for generating migrated config

--- test/main/store/state/index.test.js
+++ test/main/store/state/index.test.js
@@ -1,10 +1,23 @@
 jest.mock('electron-log', () => ({ info: console.log, error: jest.fn() }))
 jest.mock('electron', () => ({ app: { on: jest.fn(), getPath: jest.fn() } }))
+jest.mock('fs')
+
+let mockLatestVersion = 0
 
 jest.mock('../../../../main/store/migrations', () => {
+  const migrations = {
+    latest: mockLatestVersion
+  }
+
+  const apply = (state) => {
+    return migrations.latest === 2
+      ? { ...state, main: { ...state.main, _version: 2, instanceId: 'test-brand-new-frame' } }
+      : { ...state }
+  }
+
   return {
-    apply: (state) => state,
-    latest: 1
+    ...migrations,
+    apply
   }
 })
 
@@ -40,8 +53,7 @@ afterEach(() => {
 
 it('maintains backwards compatible access to the current version of state', async () => {
   // load state already migrated to version 2 and make sure version 1 values are available
-  const { default: migrations } = await import('../../../../main/store/migrations')
-  migrations.latest = 1
+  mockLatestVersion = 1
 
   const { default: state } = await import('../../../../main/store/state')
 
@@ -50,10 +62,25 @@ it('maintains backwards compatible access to the current version of state', asyn
 
 it('loads values from the current version of the state', async () => {
   // load state already migrated to version 2 and make sure version 1 values are available
-  const { default: migrations } = await import('../../../../main/store/migrations')
-  migrations.latest = 2
+  mockLatestVersion = 2
 
   const { default: state } = await import('../../../../main/store/state')
 
   expect(state().main.instanceId).toBe('test-brand-new-frame')
 })
+
+it('preserves an older version of the state after creating a newer state entry', async () => {
+  mockLatestVersion = 2
+
+  jest.dontMock('../../../../main/store/persist')
+  const { default: fs } = await import('fs')
+  const { default: persist } = await import('../../../../main/store/persist')
+  const { default: state } = await import('../../../../main/store/state')
+
+  persist.set('main', state().main)
+
+  const writtenState = JSON.parse(fs.__getWrittenData())
+
+  expect(writtenState.main.__['1'].main.instanceId).toBe('test-frame')
+  expect(writtenState.main.__['2'].main.instanceId).toBe('test-brand-new-frame')
+}, 500)
