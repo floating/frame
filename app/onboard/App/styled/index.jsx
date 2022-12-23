cf2dba864b2d4c976155fba336df8da3e35237fe
Jordan Muir <jordan@frame.sh>
2022-12-23 17:01:39 -0500

    fix tests

--- app/onboard/App/styled/index.jsx
+++ app/onboard/App/styled/index.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styled from 'styled-components'
 import Balancer from 'react-wrap-balancer'
 
--- test/app/onboard/App/Slides/index.test.js
+++ test/app/onboard/App/Slides/index.test.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
 import { setupComponent } from '../../../../componentSetup'
-import Slides from '../../../../../app/onboard/App/Slides'
+import Proceed from '../../../../../app/onboard/App/Slides/Proceed'
 
 it('completes when the user clicks close', async () => {
   const onComplete = jest.fn()
-  const { user, getByRole } = setupComponent(<Slides slide={6} onComplete={onComplete} />)
+  const { user, getByRole } = setupComponent(
+    <Proceed
+      slide={7}
+      proceed={{ action: 'complete', text: 'Done' }}
+      nextSlide={() => {}}
+      prevSlide={() => {}}
+      onComplete={onComplete}
+    />
+  )
 
-  await user.click(getByRole('button', { name: 'Close' }))
+  await user.click(getByRole('button', { name: 'Done' }))
 
   expect(onComplete).toHaveBeenCalled()
 })
