f0c5b6a9a5976b6c16a53064c822cd7473696aad
Matt Holtzman <matt.holtzman@gmail.com>
2023-01-02 12:21:49 -0500

    fix for undefined notice

---
+++ test/app/Tray/Account/Account/Requests/TransactionRequest/index.test.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import Restore from 'react-restore'
+import { screen } from '@testing-library/dom'
+
+import store from '../../../../../../../main/store'
+import { setupComponent } from '../../../../../../componentSetup'
+import TxRequestComponent from '../../../../../../../app/tray/Account/Account/Requests/TransactionRequest'
+
+jest.mock('../../../../../../../main/store/persist')
+jest.mock('../../../../../../../resources/link', () => ({ rpc: jest.fn() }))
+
+const TxRequest = Restore.connect(TxRequestComponent, store)
+
+const account = '0xDAFEA492D9c6733ae3d56b7Ed1ADB60692c98Bc5'
+
+function addRequest(req) {
+  store.updateAccount({
+    id: account,
+    name: 'Test Account',
+    requests: {
+      [req.id]: req
+    }
+  })
+}
+
+describe('confirm', () => {
+  it('renders a confirming transaction', () => {
+    const req = {
+      id: 'test-req',
+      status: 'confirming',
+      data: {
+        chainId: '0x89'
+      }
+    }
+
+    addRequest(req)
+
+    setupComponent(<TxRequest step='confirm' handlerId={req.id} accountId={account} />)
+  })
+
+  it.only('renders a transaction with insufficient funds for gas', () => {
+    const req = {
+      id: 'test-req',
+      type: 'transaction',
+      status: 'confirming',
+      notice: 'insufficient funds for gas',
+      recipientType: 'external',
+      data: {
+        chainId: '0x89'
+      }
+    }
+
+    addRequest(req)
+
+    setupComponent(<TxRequest step='confirm' handlerId={req.id} accountId={account} />)
+
+    const warning = screen.getByRole('alert')
+    expect(warning.textContent).toMatch(/insufficient funds for gas/i)
+  })
+})
