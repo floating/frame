290b07d015d7baa1cf082a9a0a32296b571e4e0d
goosewobbler <goosewobbler@pm.me>
2023-01-13 14:27:31 +0000

    revert isDefaultAccountName change

--- resources/domain/account/index.ts
+++ resources/domain/account/index.ts
@@ -2,23 +2,8 @@ import { getSignerDisplayType } from '../signer'
 
 export const accountNS = '114c39e5-cd7d-416f-ab9e-5ab6ab0218ce'
 
-export const isDefaultAccountName = ({ name, lastSignerType }: Account) => {
-  if (!lastSignerType) {
-    return false
-  }
-  const signerDisplayTypeMap = {
-    ring: () => name.toLowerCase() === 'hot account',
-    seed: () => name.toLowerCase() === 'hot account',
-    address: () => ['address account', 'watch account'].includes(name.toLowerCase()), // address type accounts can be displayed as 'address' (beta.22) or 'watch' accounts (omnichain onwards)
-    trezor: () => name.toLowerCase() === 'trezor account',
-    ledger: () => name.toLowerCase() === 'ledger account',
-    lattice: () => name.toLowerCase() === 'lattice account'
-  }
-  const signerDisplayTypeCheckFunc =
-    signerDisplayTypeMap[lastSignerType.toLowerCase() as keyof typeof signerDisplayTypeMap]
-
-  return signerDisplayTypeCheckFunc ? signerDisplayTypeCheckFunc() : false
-}
+export const isDefaultAccountName = ({ name, lastSignerType }: Account) =>
+  name.toLowerCase() === getDefaultAccountName(lastSignerType)
 
 export const getDefaultAccountName = (type: string) => `${getSignerDisplayType(type)} account`
 
