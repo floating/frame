4f2a37e091c2fa6edb1f82162aeacae8f311be64
goosewobbler <goosewobbler@pm.me>
2023-01-13 19:34:55 +0000

    delete unused

--- main/crypt/index.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto')
-
-const stringToKey = (pass) => {
-  const hash = crypto.createHash('sha256').update(pass)
-  return Buffer.from(hash.digest('hex').substring(0, 32))
-}
-
-module.exports = { stringToKey }
