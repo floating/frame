889ac33ef79f30b75d4d97234c1e584878c20759
goosewobbler <goosewobbler@protonmail.com>
2023-02-08 13:08:29 +0000

    allow advanceTimersAfterInput to be a number, pass through

--- test/componentSetup.js
+++ test/componentSetup.js
@@ -14,7 +14,10 @@ async function actAndWait(fn, ms = 0) {
 function setupComponent(jsx, opts = {}) {
   const { advanceTimersAfterInput = false, ...options } = opts
   const advanceTimers =
-    options.advanceTimers || (advanceTimersAfterInput ? () => jest.runAllTimers() : advanceTimersByTime)
+    options.advanceTimers ||
+    (advanceTimersAfterInput === true
+      ? () => jest.runAllTimers()
+      : () => advanceTimersByTime(advanceTimersAfterInput || 0))
 
   return {
     user: userEvent.setup({
