6ed07ca5c72143fc29ec238b0a6547ff82276a39
Jordan Muir <jordan@frame.sh>
2023-02-12 14:47:13 -0500

    cache get

--- resources/app/index.js
+++ resources/app/index.js
@@ -1,10 +1,10 @@
-let keyboardLayout
-navigator.keyboard.getLayoutMap().then((_keyboardLayout) => {
-  keyboardLayout = _keyboardLayout
+let mappedSlash
+navigator.keyboard.getLayoutMap().then((keyboardLayout) => {
+  mappedSlash = keyboardLayout.get('Slash')
 })
 
 export const getSummonShortcut = (platform) => {
   const modifierKey = platform === 'darwin' ? 'Option' : 'Alt'
-  const summonKey = keyboardLayout ? keyboardLayout.get('Slash') : '/'
+  const summonKey = mappedSlash ? mappedSlash : '/'
   return { modifierKey, summonKey }
 }
