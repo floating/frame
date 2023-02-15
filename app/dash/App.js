0445e935d7db989654d72065bce6d45f8855f515
Matt Holtzman <matt.holtzman@gmail.com>
2023-02-14 19:28:01 -0500

    use key for signer to force re-mount when changing signers

--- app/dash/App.js
+++ app/dash/App.js
@@ -51,8 +51,10 @@ class Dash extends React.Component {
   renderPanel(view, data) {
     if (view === 'accounts') return <Accounts data={data} />
     if (view === 'expandedSigner' && data.signer) {
-      const signer = this.store('main.signers', data.signer)
-      return <Signer expanded={true} {...signer} />
+      const signerId = data.signer
+      const signer = this.store('main.signers', signerId)
+
+      return <Signer key={signerId} expanded={true} {...signer} />
     }
     if (view === 'chains') return <Chains data={data} />
     if (view === 'dapps') return <Dapps data={data} />
