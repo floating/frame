b37258b11b2ab1a2e6df2d69ab5e0311dfc59f90
Matt Holtzman <matt.holtzman@gmail.com>
2023-03-21 13:53:06 -0400

    add v37 schema

--- main/store/migrate/migrations/37/index.ts
+++ main/store/migrate/migrations/37/index.ts
@@ -1,23 +1,29 @@
-// TODO: remove this once implemented
 import log from 'electron-log'
 
-// TODO: remove this once implemented
 import { v35StateSchema } from '../35/schema'
+
+import type { v37State } from './schema'
 
 const migrate = (initial: unknown) => {
   try {
-    // TODO: implement parsing and migration
-    // const state = v35StateSchema.parse(initial)
-    const state = initial as any
+    const state = v35StateSchema.parse(initial)
     const summonShortcutEnabled = state.main.shortcuts.altSlash
-    state.main.shortcuts = {
-      summon: {
-        modifierKeys: ['Alt'],
-        shortcutKey: 'Slash',
-        enabled: summonShortcutEnabled
+
+    const migratedState: v37State = {
+      ...state,
+      main: {
+        ...state.main,
+        shortcuts: {
+          summon: {
+            modifierKeys: ['Alt'],
+            shortcutKey: 'Slash',
+            enabled: summonShortcutEnabled
+          }
+        }
       }
     }
-    return state
+
+    return migratedState
   } catch (e) {
     log.error('Migration 37: could not parse state', e)
   }
---
+++ main/store/migrate/migrations/37/schema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+import { v35MainSchema, v35StateSchema } from '../35/schema'
+
+const v37ShortcutsSchema = z.object({
+  summon: z.object({
+    modifierKeys: z.array(z.enum(['Alt', 'Ctrl', 'Meta', 'Cmd'])),
+    shortcutKey: z.string(),
+    enabled: z.boolean()
+  })
+})
+
+const v37MainSchema = v35MainSchema.merge(z.object({ shortcuts: v37ShortcutsSchema }))
+const mainUpdates = z.object({ main: v37MainSchema }).passthrough()
+
+export const v37StateSchema = v35StateSchema.merge(mainUpdates).passthrough()
+
+export type v37State = z.infer<typeof v37StateSchema>
