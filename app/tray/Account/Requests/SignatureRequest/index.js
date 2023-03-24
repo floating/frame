5702157a0eb66639d04dc3bbf1f8af77c55a65d7
Jamie M <54599757+Jamchello@users.noreply.github.com>
2023-03-24 11:46:00 +0000

    if rawMessage cannot be decoded to valid utf-8, show the raw hex (#1486)
    * if rawMessage cannot be decoded to valid utf-8, show the raw hex
    
    * use version 4.0.0 of isUtf8
    
    * run install again to update package-lock
    
    * change component to functional, add test
    
    * use textdecoder
    
    * decode message in main process
    
    ---------
    
    Co-authored-by: Matt Holtzman <matt.holtzman@gmail.com>

--- app/tray/Account/Requests/SignatureRequest/index.js
+++ app/tray/Account/Requests/SignatureRequest/index.js
@@ -1,74 +1,56 @@
-import React from 'react'
-import Restore from 'react-restore'
-import { isHexString } from '@ethersproject/bytes'
-import { stripHexPrefix } from './../../../../../resources/utils'
+import React, { useRef } from 'react'
 
-function decodeMessage(rawMessage) {
-  if (isHexString(rawMessage)) {
-    const buff = Buffer.from(stripHexPrefix(rawMessage), 'hex')
-    return buff.length === 32 ? rawMessage : buff.toString('utf8')
-  }
+function getRequestClass(status) {
+  let requestClass = 'signerRequest'
+  if (status === 'success') requestClass += ' signerRequestSuccess'
+  if (status === 'declined') requestClass += ' signerRequestDeclined'
+  if (status === 'pending') requestClass += ' signerRequestPending'
+  if (status === 'error') requestClass += ' signerRequestError'
 
-  // replace all multiple line returns with just one to prevent excess space in message
-  return rawMessage.replaceAll(/[\n\r]+/g, '\n')
+  return requestClass
 }
 
-class TransactionRequest extends React.Component {
-  constructor(...args) {
-    super(...args)
-    this.state = { allowInput: false, dataView: false }
+const Message = ({ text }) => {
+  const outerRef = useRef(null)
+  const innerRef = useRef(null)
 
-    const props = args[0] || {}
+  const shouldShowMore = () => {
+    if (!outerRef.current || !innerRef.current) return false
 
-    this.signRefs = [React.createRef(), React.createRef()]
-
-    setTimeout(() => {
-      this.setState({ allowInput: true })
-    }, props.signingDelay || 1500)
+    const inner = innerRef.current.clientHeight
+    const wrap = outerRef.current.clientHeight + outerRef.current.scrollTop
+    return inner > wrap
   }
 
-  renderMessage(message) {
-    let showMore = false
-    if (this.signRefs[0].current && this.signRefs[1].current) {
-      const inner = this.signRefs[1].current.clientHeight
-      const wrap = this.signRefs[0].current.clientHeight + this.signRefs[0].current.scrollTop
-      if (inner > wrap) showMore = true
-    }
-    return (
-      <div ref={this.signRefs[0]} className='signValue'>
-        <div ref={this.signRefs[1]} className='signValueInner'>
-          {message}
-        </div>
-        {showMore ? <div className='signValueMore'>scroll to see more</div> : null}
+  return (
+    <div ref={outerRef} className='signValue'>
+      <div ref={innerRef} className='signValueInner'>
+        {text}
       </div>
-    )
-  }
+      {shouldShowMore() ? <div className='signValueMore'>scroll to see more</div> : null}
+    </div>
+  )
+}
 
-  render() {
-    const type = this.props.req.type
-    const status = this.props.req.status
-    const payload = this.props.req.payload
+const MessageToSign = ({ req }) => {
+  const { id, handlerId, type, status } = req
 
-    const message = decodeMessage(payload.params[1])
+  const message = req.data.decodedMessage
+  const requestClass = getRequestClass(status)
 
-    let requestClass = 'signerRequest'
-    if (status === 'success') requestClass += ' signerRequestSuccess'
-    if (status === 'declined') requestClass += ' signerRequestDeclined'
-    if (status === 'pending') requestClass += ' signerRequestPending'
-    if (status === 'error') requestClass += ' signerRequestError'
-
-    return (
-      <div key={this.props.req.id || this.props.req.handlerId} className={requestClass}>
-        {type === 'sign' ? (
-          <div className='approveRequest'>
-            <div className='approveTransactionPayload'>{this.renderMessage(message)}</div>
+  return (
+    <div key={id || handlerId} className={requestClass}>
+      {type === 'sign' ? (
+        <div className='approveRequest'>
+          <div className='approveTransactionPayload'>
+            <Message text={message} />
           </div>
-        ) : (
-          <div className='unknownType'>{'Unknown: ' + this.props.req.type}</div>
-        )}
-      </div>
-    )
-  }
+        </div>
+      ) : (
+        <div className='unknownType'>{'Unknown: ' + type}</div>
+      )}
+    </div>
+  )
 }
 
-export default Restore.connect(TransactionRequest)
+export default MessageToSign
