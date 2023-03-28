55d94b21697ed5f931d8525ec30369037ccbc879
goosewobbler <goosewobbler@protonmail.com>
2023-03-28 21:46:59 +0100

    fix commands

--- scripts/update-canary-version.sh
+++ scripts/update-canary-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "NEW_VERSION=$(grep -o '[[:digit:]].[[:digit:]].[[:digit:]]-canary.[[:digit:]]' package.json | awk -F. '{$NF = $NF + 1;} 1' OFS=.)" >> $GITHUB_OUTPUT
-sed -i "s/[[:digit:]].[[:digit:]].[[:digit:]]-canary.[[:digit:]]/${{ steps.update-version.outputs.NEW_VERSION }}/" package.json
+NEW_VERSION="$(grep -o '\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]-canary.[[:digit:]]\"' package.json | awk -F. '{$NF = $NF + 1;} 1' OFS=.)\""
+sed -i "s/\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]-canary.[[:digit:]]\"/$NEW_VERSION/" package.json
--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "NEW_VERSION=$(grep -o '\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]\"' package.json | awk -F. '{$NF = $NF + 1;} 1' OFS=.)" >> $GITHUB_OUTPUT
-sed -i "s/\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]\"/${{ steps.update-version.outputs.NEW_VERSION }}/" package.json
+NEW_VERSION="$(grep -o '\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]\"' package.json | awk -F. '{$NF = $NF + 1;} 1' OFS=.)\""
+sed -i "s/\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]\"/$NEW_VERSION/" package.json
