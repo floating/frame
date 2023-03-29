c008ec44cf85c93b627e8b4deb858ca0a4268050
goosewobbler <goosewobbler@protonmail.com>
2023-03-29 23:35:41 +0100

    update

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NEW_CANARY_VERSION="$(grep -o '\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]-canary.[[:digit:]]\"' package.json | awk -F. '{$NF = $NF + 1;} 1' OFS=.)\""
-if $NEW_CANARY_VERSION != ''; then
+if [ "$NEW_CANARY_VERSION" != '' ]; then
   sed -i "s/\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]-canary.[[:digit:]]\"/$NEW_CANARY_VERSION/" package.json
 else
   NEW_VERSION="$(grep -o '\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]\"' package.json | awk -F. '{$NF = $NF + 1;} 1' OFS=.)\""
