1adc93416494249b76ba14e6d4fdb74fa80cef90
goosewobbler <goosewobbler@protonmail.com>
2023-03-29 12:26:24 +0100

    return chainId when enabled regardless of connection status

--- main/provider/index.ts
+++ main/provider/index.ts
@@ -185,23 +185,19 @@ export class Provider extends EventEmitter {
   }
 
   getNetVersion(payload: RPCRequestPayload, res: RPCRequestCallback, targetChain: Chain) {
-    const connection = this.connection.connections[targetChain.type][targetChain.id]
-    const chainConnected = connection && (connection.primary?.connected || connection.secondary?.connected)
-
-    const response = chainConnected
-      ? { result: connection.chainId }
-      : { error: { message: 'not connected', code: 1 } }
+    const chain = store('main.networks.ethereum', targetChain.id)
+    const response = chain?.enabled
+      ? { result: targetChain.id }
+      : { error: { message: !chain ? 'chain not present' : 'chain not enabled', code: 1 } }
 
     res({ id: payload.id, jsonrpc: payload.jsonrpc, ...response })
   }
 
   getChainId(payload: RPCRequestPayload, res: RPCSuccessCallback, targetChain: Chain) {
-    const connection = this.connection.connections[targetChain.type][targetChain.id]
-    const chainConnected = connection.primary?.connected || connection.secondary?.connected
-
-    const response = chainConnected
+    const chain = store('main.networks.ethereum', targetChain.id)
+    const response = chain?.enabled
       ? { result: intToHex(targetChain.id) }
-      : { error: { message: 'not connected', code: 1 } }
+      : { error: { message: !chain ? 'chain not present' : 'chain not enabled', code: 1 } }
 
     res({ id: payload.id, jsonrpc: payload.jsonrpc, ...response })
   }
