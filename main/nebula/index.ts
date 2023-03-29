f62c25e303f7a31637c87ac7cf27a3dc61d02f8b
goosewobbler <goosewobbler@protonmail.com>
2023-03-29 21:47:26 +0100

    add comments to clarify

--- main/nebula/index.ts
+++ main/nebula/index.ts
@@ -1,16 +1,20 @@
-// delete the Electron version while requiring Nebula. this allows ipfs-utils to use
-// node-fetch instead of electron-fetch
+// store electron version
 const electron = process.versions.electron
 
+// Mutable type removes readonly attribute from properties so TS doesn't complain when deleting them
 type Mutable<T> = {
   -readonly [key in keyof T]?: T[key]
 }
 
+// Delete the electron version while requiring Nebula. This allows ipfs-utils to use
+// node-fetch instead of electron-fetch - can remove this when ipfs-utils supports ELECTRON_RUN_AS_NODE
+// https://github.com/ipfs/js-ipfs-utils/issues/140
 const versions = process.versions as Mutable<NodeJS.ProcessVersions>
 delete versions.electron
 
 import nebula from 'nebula'
 
+// reinstate original electron version
 versions.electron = electron
 
 import EthereumProvider from 'ethereum-provider'
