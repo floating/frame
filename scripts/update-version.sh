5b8de6a4cc943dc57795c54a7b9e00f2194cc8f1
goosewobbler <goosewobbler@protonmail.com>
2023-03-30 17:34:35 +0100

    improve script resiliency

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-VERSION=$(echo "$GITHUB_REF" | sed -r 's/v([0-9]\.[0-9]\.[0-9][-canary\.[0-9]]?)/\1/g')
-NEW_VERSION=$(echo "$VERSION" | awk -F. '{$NF = $NF + 1;} 1' OFS=.)
-
-if [[ $VERSION == *'canary'* ]]; then
-  echo "Updating Canary: $NEW_VERSION"
-  sed -i "s/\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]-canary.[[:digit:]]\"/\"version\": \"$NEW_VERSION\"/" package.json
+if [[ "$GITHUB_REF" =~ ^v[0-9]\.[0-9]\.[0-9](-canary\.[0-9])?$ ]]; then
+  VERSION=$(echo "$GITHUB_REF" | sed -r 's/v([0-9]\.[0-9]\.[0-9](-canary\.[0-9])?)/\1/g')
+  NEW_VERSION=$(echo "$VERSION" | awk -F. '{$NF = $NF + 1;} 1' OFS=.)
+  
+  if [[ $VERSION == *'canary'* ]]; then
+    echo "Updating Canary: $NEW_VERSION"
+    sed -i "s/\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]-canary.[[:digit:]]\"/\"version\": \"$NEW_VERSION\"/" package.json
+  else
+    echo "Updating Production: $NEW_VERSION"
+    sed -i "s/\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]\"/\"version\": \"$NEW_VERSION\"/" package.json
+  fi
 else
-  echo "Updating Production: $NEW_VERSION"
-  sed -i "s/\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]\"/\"version\": \"$NEW_VERSION\"/" package.json
+  echo "Could not update for release tag $GITHUB_REF"
 fi
