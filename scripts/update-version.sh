504add195ec05027b70629f4d0405e93004e0299
goosewobbler <goosewobbler@protonmail.com>
2023-03-30 17:56:14 +0100

    update

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [[ "$GITHUB_REF" =~ ^v[0-9]\.[0-9]\.[0-9](-canary\.[0-9])?$ ]]; then
-  VERSION=$(echo "$GITHUB_REF" | sed -r 's/v([0-9]\.[0-9]\.[0-9](-canary\.[0-9])?)/\1/g')
+if [[ "$GITHUB_REF" =~ ^(.*)v[0-9]\.[0-9]\.[0-9](-canary\.[0-9])?$ ]]; then
+  VERSION=$(echo "$GITHUB_REF" | sed -r 's/(.*)v([0-9]\.[0-9]\.[0-9](-canary\.[0-9])?)/\2/g')
   NEW_VERSION=$(echo "$VERSION" | awk -F. '{$NF = $NF + 1;} 1' OFS=.)
   
   if [[ $VERSION == *'canary'* ]]; then
-    echo "Updating Canary: $NEW_VERSION"
+    echo "Updating Canary to \"$NEW_VERSION\""
     sed -i "s/\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]-canary.[[:digit:]]\"/\"version\": \"$NEW_VERSION\"/" package.json
   else
-    echo "Updating Production: $NEW_VERSION"
+    echo "Updating Production to \"$NEW_VERSION\""
     sed -i "s/\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]\"/\"version\": \"$NEW_VERSION\"/" package.json
   fi
 else
