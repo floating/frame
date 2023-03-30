462daccacb3e53836acd3e1d316349c578b00e96
goosewobbler <goosewobbler@protonmail.com>
2023-03-30 17:12:33 +0100

    update for passing ref in

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ "$(grep -o 'canary' package.json)" ]; then
-  NEW_CANARY_VERSION="$(grep -o '\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]-canary.[[:digit:]]\"' package.json | awk -F. '{$NF = $NF + 1;} 1' OFS=.)\""
-  echo "Updating Canary: $NEW_CANARY_VERSION"
-  sed -i "s/\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]-canary.[[:digit:]]\"/$NEW_CANARY_VERSION/" package.json
+VERSION=$(echo "$GITHUB_REF" | sed -r 's/v([0-9]\.[0-9]\.[0-9][-canary\.[0-9]]?)/\1/g')
+NEW_VERSION=$(echo "$VERSION" | awk -F. '{$NF = $NF + 1;} 1' OFS=.)
+
+if [[ $VERSION == *'canary'* ]]; then
+  echo "Updating Canary: $NEW_VERSION"
+  sed -i "s/\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]-canary.[[:digit:]]\"/\"version\": \"$NEW_VERSION\"/" package.json
 else
-  NEW_VERSION="$(grep -o '\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]\"' package.json | awk -F. '{$NF = $NF + 1;} 1' OFS=.)\""
   echo "Updating Production: $NEW_VERSION"
-  sed -i "s/\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]\"/$NEW_VERSION/" package.json
+  sed -i "s/\"version\": \"[[:digit:]].[[:digit:]].[[:digit:]]\"/\"version\": \"$NEW_VERSION\"/" package.json
 fi
