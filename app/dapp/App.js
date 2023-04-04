9ecd5e369c24d3fd0ced71681ce5c87138586a36
goosewobbler <goosewobbler@protonmail.com>
2023-04-04 17:31:16 +0100

    add view chains button

--- app/dapp/App.js
+++ app/dapp/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Restore from 'react-restore'
 
+import link from '../../resources/link'
 import Native from '../../resources/Native'
 
 class App extends React.Component {
@@ -30,10 +31,9 @@ class App extends React.Component {
     const sendDapp =
       this.store('main.dapps', '0xe8d705c28f65bc3fe10df8b22f9daa265b99d0e1893b2df49fd38120f0410bca') || {}
 
-    const isMainnetConnected = () => {
-      const mainnet = this.store('main.networks.ethereum.1')
-      return mainnet.on && (mainnet.connection.primary.connected || mainnet.connection.secondary.connected)
-    }
+    const mainnet = this.store('main.networks.ethereum.1')
+    const isMainnetConnected =
+      mainnet.on && (mainnet.connection.primary.connected || mainnet.connection.secondary.connected)
 
     return (
       <div className='splash'>
@@ -43,10 +43,23 @@ class App extends React.Component {
           {!currentView.ready && sendDapp.status === 'failed' ? (
             <div className='mainDappLoading'>
               <div className='mainDappLoadingText'>
-                {isMainnetConnected()
+                {isMainnetConnected
                   ? 'Send dapp failed to load'
                   : 'Mainnet connection required to resolve ENS'}
               </div>
+              {!isMainnetConnected && (
+                <div
+                  className='mainDappEnableChains'
+                  onClick={() => {
+                    link.send('tray:action', 'navDash', { view: 'chains', data: {} })
+                    setTimeout(() => {
+                      link.send('frame:close')
+                    }, 100)
+                  }}
+                >
+                  View Chains
+                </div>
+              )}
             </div>
           ) : null}
         </div>
