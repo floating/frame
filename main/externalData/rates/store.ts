421aee5331984093d45869059b45ca3f1228fc60
Jamie M <54599757+Jamchello@users.noreply.github.com>
2023-05-19 01:11:16 +0800

    only show rates which have updated in the last 5 minutes (#1558)
    * add rates manager and appropriate tests
    
    * rename modules
    
    * tests running again
    
    * address PR comments
    
    * cleanup
    
    * last cleanup
    
    * fix bug in action name
    
    * last cleanup I promise
    
    ---------
    
    Co-authored-by: Matt Holtzman <matt.holtzman@gmail.com>

---
+++ main/externalData/rates/store.ts
@@ -0,0 +1,96 @@
+import log from 'electron-log'
+import { AssetId, AssetType } from '@framelabs/pylon-client/dist/assetId'
+
+import store from '../../store'
+
+import type { UsdRate } from '../../provider/assets'
+import type { Rate } from '../../store/state'
+
+type RateUpdate = {
+  id: AssetId
+  data: {
+    usd: number
+    usd_24h_change: number
+  }
+}
+
+const RATES_EXPIRY = 1000 * 5 * 60 // rates are valid for 5 minutes before being considered stale
+const timeouts: Record<string, NodeJS.Timer> = {}
+
+const separateUpdates = (updates: RateUpdate[]) =>
+  updates.reduce(
+    (acc, update) => {
+      return update.id.type === AssetType.NativeCurrency
+        ? { ...acc, native: [...acc.native, update] }
+        : { ...acc, token: [...acc.token, update] }
+    },
+    { native: [] as RateUpdate[], token: [] as RateUpdate[] }
+  )
+
+const gatherTokenRates = (updates: RateUpdate[]) =>
+  updates.reduce((rates, update) => {
+    const address = update.id.address as string
+
+    rates[address] = {
+      usd: {
+        price: update.data.usd,
+        change24hr: update.data.usd_24h_change
+      }
+    }
+
+    return rates
+  }, {} as Record<string, UsdRate>)
+
+const storeApi = {
+  setTokenRates: (rates: Record<Address, UsdRate>) => store.setRates(rates),
+  removeTokenRate: (address: Address) => store.removeRate(address),
+  setNativeCurrencyRate: (chainId: number, rate: Rate) =>
+    store.setNativeCurrencyData('ethereum', chainId, { usd: rate }),
+  removeNativeCurrencyRate: (chainId: number) => store.removeNativeCurrencyRate('ethereum', chainId)
+}
+
+const handleNativeCurrencyUpdates = (updates: RateUpdate[]) => {
+  log.debug(`got currency rate updates for chains: ${updates.map((u) => u.id.chainId)}`)
+
+  updates.forEach((u) => {
+    storeApi.setNativeCurrencyRate(u.id.chainId, {
+      price: u.data.usd,
+      change24hr: u.data.usd_24h_change
+    })
+
+    const id = `native:${u.id.chainId}`
+    const expiry = setTimeout(() => storeApi.removeNativeCurrencyRate(u.id.chainId), RATES_EXPIRY)
+
+    clearTimeout(timeouts[id])
+    timeouts[id] = expiry
+  })
+}
+
+const handleTokenUpdates = (updates: RateUpdate[]) => {
+  log.debug(`got token rate updates for addresses: ${updates.map((u) => u.id.address)}`)
+
+  const rates = gatherTokenRates(updates)
+  storeApi.setTokenRates(rates)
+
+  updates.forEach((u) => {
+    const address = u.id.address as string
+    const expiry = setTimeout(() => storeApi.removeTokenRate(address), RATES_EXPIRY)
+
+    clearTimeout(timeouts[address])
+    timeouts[address] = expiry
+  })
+}
+
+const handleUpdates = (rates: RateUpdate[]) => {
+  const { token, native } = separateUpdates(rates)
+
+  if (native.length) {
+    handleNativeCurrencyUpdates(native)
+  }
+
+  if (token.length) {
+    handleTokenUpdates(token)
+  }
+}
+
+export { handleUpdates }
--- main/store/actions/index.js
+++ main/store/actions/index.js
@@ -149,7 +149,8 @@ module.exports = {
       const balances = { ...account.balances, populatedChains }
       const updated = { ...account, balances }
 
-      log.verbose('addPopulatedChains', { address, chains, updated, populatedChains })
+      log.debug('addPopulatedChains', { address, chains, updated, populatedChains })
+
       return updated
     })
   },
@@ -283,6 +284,9 @@ module.exports = {
   },
   setNativeCurrencyData: (u, netType, netId, currency) => {
     u('main.networksMeta', netType, netId, 'nativeCurrency', (existing) => ({ ...existing, ...currency }))
+  },
+  removeNativeCurrencyRate: (u, netType, netId) => {
+    u('main.networksMeta', netType, netId, 'nativeCurrency', ({ usd, ...currency }) => currency)
   },
   addNetwork: (u, net) => {
     try {
@@ -583,6 +587,12 @@ module.exports = {
   },
   setRates: (u, rates) => {
     u('main.rates', (existingRates = {}) => ({ ...existingRates, ...rates }))
+  },
+  removeRate: (u, contractAddress) => {
+    u('main.rates', (rates = {}) => {
+      delete rates[contractAddress]
+      return rates
+    })
   },
   // Inventory
   setInventory: (u, address, inventory) => {
