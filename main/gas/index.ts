a50d8ce0dec3ae7c6a21ad3dbb8234e7537ef4d2
Sam Maister <goosewobbler@protonmail.com>
2023-05-24 16:52:17 +0100

    add TODO

--- main/gas/index.ts
+++ main/gas/index.ts
@@ -194,6 +194,7 @@ const gasChainMap = {
   80001: PolygonGas
 }
 
+// TODO: rationalise use of provider / provider.connection
 export function init(provider: Provider, chainIdStr: string) {
   const chainId = parseInt(chainIdStr)
   const ChainSpecificGas = gasChainMap[chainId as keyof typeof gasChainMap]
