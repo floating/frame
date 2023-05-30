cde5535d255388752cf971687ecff2cbf420e769
Jordan Muir <jordan@frame.sh>
2023-05-30 11:00:07 -0400

    organize styled components

--- app/tray/Account/Inventory/CollectionList/index.js
+++ app/tray/Account/Inventory/CollectionList/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import styled, { keyframes, css } from 'styled-components'
 
 import link from '../../../../../resources/link'
 import RingIcon from '../../../../../resources/Components/RingIcon'
@@ -8,101 +7,15 @@ import { ClusterRow, ClusterValue } from '../../../../../resources/Components/Cl
 import RingIcon from '../../../../../resources/Components/RingIcon'
 import useStore from '../../../../../resources/Hooks/useStore'
 
-const CollectionInner = styled.div`
-  position: relative;
-  height: 65px;
-  transition: all linear 0.8s;
-`
-
-const CollectionIcon = styled.div`
-  position: absolute;
-  top: 14px;
-  left: 14px;
-`
-
-const CollectionMain = styled.div`
-  position: absolute;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  inset: 30px 20px 12px 66px;
-`
-
-const CollectionLine = styled.div`
-  background: var(--ghostY);
-  height: 1px;
-  margin: 0px 12px 0px 6px;
-  flex: 1;
-  position: relative;
-`
-
-const wave = keyframes`
-  0% { 
-    transform: translate(0, 0);
-  }
-  50% {
-    transform: translate(1px, 2px);
-  }
-  100% { 
-    transform: translate(0, 0);
-  }
-`
-
-const CollectionDot = styled.div`
-  display: flex;
-  height: 16px;
-  width: 16px;
-  min-height: 16px;
-  min-width: 16px;
-  margin-right: 6px;
-  border-radius: 4px;
-  transition: var(--standard);
-  overflow: hidden;
-  justify-content: center;
-  position: relative;
-  font-family: 'FiraCode';
-  font-size: 10px;
-  border: 1px solid var(--ghostB);
-  background: var(--ghostB);
-  box-shadow: 0px 1px 2px var(--ghostY);
-  z-index: 10;
-  img {
-    margin: -1px;
-    object-fit: cover;
-    position: relative;
-    z-index: 10;
-  }
-  img:before {
-    content: attr(alt);
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    overflow: hidden;
-    background: var(--ghostB);
-    color: var(--outerspace);
-    color: transparent;
-  }
-`
-
-const CollectionDots = styled.div`
-  display: flex;
-  align-items: center;
-  height: 100%;
-  transition: var(--standardFast);
-`
-
-const CollectionCount = styled.div`
-  display: flex;
-  height: 20px;
-  min-height: 20px;
-  justify-content: center;
-  align-items: center;
-  font-size: 16px;
-  font-family: 'FiraCode';
-  padding-left: 4px;
-`
+import {
+  CollectionInner,
+  CollectionIcon,
+  CollectionMain,
+  CollectionLine,
+  CollectionDot,
+  CollectionDots,
+  CollectionCount
+} from './styled'
 
 const displayName = (name = '') => {
   if (name.length > 19) {
