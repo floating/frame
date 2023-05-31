85a3ac87294b7ab0a5b40127165fb3ddbe41afff
Sam Maister <goosewobbler@protonmail.com>
2023-05-31 12:14:17 +0100

    remove populate & maxfee

--- main/transaction/index.ts
+++ main/transaction/index.ts
@@ -70,66 +70,6 @@ function londonToLegacy(txData: TransactionData): TransactionData {
   return txData
 }
 
-
-function calculateMaxFeePerGas(maxBaseFee: string, maxPriorityFee: string) {
-  const maxFeePerGas = BigNumber(maxPriorityFee).plus(maxBaseFee).toString(16)
-  return addHexPrefix(maxFeePerGas)
-}
-
-function populate(rawTx: TransactionData, chainConfig: Common, gas: Gas): TransactionData {
-  const txData: TransactionData = { ...rawTx }
-
-  // non-EIP-1559 case
-  if (!chainConfig.isActivatedEIP(1559) || !gas.price.fees) {
-    txData.type = intToHex(chainConfig.isActivatedEIP(2930) ? 1 : 0)
-
-    const useFrameGasPrice = !rawTx.gasPrice || isNaN(parseInt(rawTx.gasPrice, 16))
-    if (useFrameGasPrice) {
-      // no valid dapp-supplied value for gasPrice so we use the Frame-supplied value
-      const gasPrice = BigNumber(gas.price.levels.fast as string).toString(16)
-      txData.gasPrice = addHexPrefix(gasPrice)
-      txData.gasFeesSource = GasFeesSource.Frame
-    }
-
-    return txData
-  }
-
-  // EIP-1559 case
-  txData.type = intToHex(2)
-
-  const useFrameMaxFeePerGas = !rawTx.maxFeePerGas || isNaN(parseInt(rawTx.maxFeePerGas, 16))
-  const useFrameMaxPriorityFeePerGas =
-    !rawTx.maxPriorityFeePerGas || isNaN(parseInt(rawTx.maxPriorityFeePerGas, 16))
-
-  if (!useFrameMaxFeePerGas && !useFrameMaxPriorityFeePerGas) {
-    // return tx unaltered when we are using no Frame-supplied values
-    return txData
-  }
-
-  if (useFrameMaxFeePerGas && useFrameMaxPriorityFeePerGas) {
-    // dapp did not supply a valid value for maxFeePerGas or maxPriorityFeePerGas so we change the source flag
-    txData.gasFeesSource = GasFeesSource.Frame
-  }
-
-  const maxPriorityFee =
-    useFrameMaxPriorityFeePerGas && gas.price.fees.maxPriorityFeePerGas
-      ? gas.price.fees.maxPriorityFeePerGas
-      : (rawTx.maxPriorityFeePerGas as string)
-
-  // if no valid dapp-supplied value for maxFeePerGas we calculate it
-  txData.maxFeePerGas =
-    useFrameMaxFeePerGas && gas.price.fees.maxBaseFeePerGas
-      ? calculateMaxFeePerGas(gas.price.fees.maxBaseFeePerGas, maxPriorityFee)
-      : txData.maxFeePerGas
-
-  // if no valid dapp-supplied value for maxPriorityFeePerGas we use the Frame-supplied value
-  txData.maxPriorityFeePerGas = useFrameMaxPriorityFeePerGas
-    ? addHexPrefix(BigNumber(maxPriorityFee).toString(16))
-    : txData.maxPriorityFeePerGas
-
-  return txData
-}
-
 function hexifySignature({ v, r, s }: Signature) {
   return {
     v: addHexPrefix(v),
