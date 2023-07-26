c161ae731710f718568c7b884614d6250c76ecc5
Matt Holtzman <matt.holtzman@gmail.com>
2023-07-26 15:51:17 -0400

    fix keyboard test

--- test/resources/Components/KeyboardShortcutConfigurator/index.test.js
+++ test/resources/Components/KeyboardShortcutConfigurator/index.test.js
@@ -40,11 +40,7 @@ it('should render an existing shortcut', () => {
     />
   )
 
-<<<<<<< HEAD
   const displayedShortcut = screen.getByLabelText('To Test this component press')
-=======
-  const displayedShortcut = screen.getByLabelText('Test this component by pressing')
->>>>>>> Choose summon keybinding (#1494)
   expect(displayedShortcut.textContent).toBe('Alt+/')
 })
 
@@ -63,11 +59,7 @@ it('should render an existing Meta key shortcut on MacOS', () => {
     />
   )
 
-<<<<<<< HEAD
   const displayedShortcut = screen.getByLabelText('To Test this component press')
-=======
-  const displayedShortcut = screen.getByLabelText('Test this component by pressing')
->>>>>>> Choose summon keybinding (#1494)
   expect(displayedShortcut.textContent).toBe('Command+/')
 })
 
@@ -85,11 +77,7 @@ it('should render an existing Alt key shortcut on MacOS', () => {
     />
   )
 
-<<<<<<< HEAD
   const displayedShortcut = screen.getByLabelText('To Test this component press')
-=======
-  const displayedShortcut = screen.getByLabelText('Test this component by pressing')
->>>>>>> Choose summon keybinding (#1494)
   expect(displayedShortcut.textContent).toBe('Option+/')
 })
 
@@ -108,11 +96,7 @@ it('should render an existing Meta key shortcut on Windows', () => {
     />
   )
 
-<<<<<<< HEAD
   const displayedShortcut = screen.getByLabelText('To Test this component press')
-=======
-  const displayedShortcut = screen.getByLabelText('Test this component by pressing')
->>>>>>> Choose summon keybinding (#1494)
   expect(displayedShortcut.textContent).toBe('Win+/')
 })
 
@@ -132,42 +116,10 @@ describe('when configuring', () => {
       />
     )
 
-<<<<<<< HEAD
     const enterShortcutPrompt = screen.getByText('Enter new keyboard shortcut!')
     expect(enterShortcutPrompt).toBeDefined()
   })
 
-=======
-    const enterShortcutPrompt = screen.getByText('Enter keyboard shortcut:')
-    expect(enterShortcutPrompt).toBeDefined()
-  })
-
-  it('should set configuring on the existing shortcut', async () => {
-    const { user } = render(
-      <KeyboardShortcutConfigurator
-        actionText='Test this component'
-        platform='linux'
-        shortcutName='Test'
-        shortcut={{
-          modifierKeys: ['Meta'],
-          shortcutKey: 'Slash',
-          enabled: true,
-          configuring: false
-        }}
-      />
-    )
-
-    const displayedShortcut = screen.getByLabelText('Test this component by pressing')
-    await user.click(displayedShortcut)
-    expect(link.send).toHaveBeenCalledWith('tray:action', 'setShortcut', 'Test', {
-      enabled: true,
-      configuring: true,
-      modifierKeys: ['Meta'],
-      shortcutKey: 'Slash'
-    })
-  })
-
->>>>>>> Choose summon keybinding (#1494)
   describe('and a valid shortcut is entered', () => {
     it('should set the new shortcut', async () => {
       const { user } = render(
@@ -184,11 +136,7 @@ describe('when configuring', () => {
         />
       )
 
-<<<<<<< HEAD
       const enterShortcutPrompt = screen.getByText('Enter new keyboard shortcut!')
-=======
-      const enterShortcutPrompt = screen.getByText('Enter keyboard shortcut:')
->>>>>>> Choose summon keybinding (#1494)
       expect(enterShortcutPrompt).toBeDefined()
       await user.keyboard('{Alt>}T{/Alt}')
 
@@ -215,11 +163,7 @@ describe('when configuring', () => {
         />
       )
 
-<<<<<<< HEAD
       const enterShortcutPrompt = screen.getByText('Enter new keyboard shortcut!')
-=======
-      const enterShortcutPrompt = screen.getByText('Enter keyboard shortcut:')
->>>>>>> Choose summon keybinding (#1494)
       expect(enterShortcutPrompt).toBeDefined()
       await user.keyboard('{Alt>}T{/Alt}')
 
@@ -248,72 +192,11 @@ describe('when configuring', () => {
         />
       )
 
-<<<<<<< HEAD
       const enterShortcutPrompt = screen.getByText('Enter new keyboard shortcut!')
-=======
-      const enterShortcutPrompt = screen.getByText('Enter keyboard shortcut:')
->>>>>>> Choose summon keybinding (#1494)
       expect(enterShortcutPrompt).toBeDefined()
       await user.keyboard('{Shift>};{/Shift}')
 
       expect(link.send).not.toHaveBeenCalled()
     })
   })
-<<<<<<< HEAD
-=======
-
-  describe('and the cancel button is clicked', () => {
-    it('should revert to displaying the existing shortcut', async () => {
-      const { user } = render(
-        <KeyboardShortcutConfigurator
-          actionText='Test this component'
-          platform='linux'
-          shortcutName='Test'
-          shortcut={{
-            modifierKeys: ['Meta'],
-            shortcutKey: 'Slash',
-            enabled: true,
-            configuring: true
-          }}
-        />
-      )
-
-      const cancelButton = screen.getByText('Cancel')
-      await user.click(cancelButton)
-      expect(link.send).toHaveBeenCalledTimes(1)
-      expect(link.send).toHaveBeenLastCalledWith('tray:action', 'setShortcut', 'Test', {
-        enabled: true,
-        configuring: false,
-        modifierKeys: ['Meta'],
-        shortcutKey: 'Slash'
-      })
-    })
-
-    it('should unset configuring on the existing shortcut', async () => {
-      const { user } = render(
-        <KeyboardShortcutConfigurator
-          actionText='Test this component'
-          platform='linux'
-          shortcutName='Test'
-          shortcut={{
-            modifierKeys: ['Meta'],
-            shortcutKey: 'Slash',
-            enabled: true,
-            configuring: true
-          }}
-        />
-      )
-
-      const cancelButton = screen.getByText('Cancel')
-      await user.click(cancelButton)
-      expect(link.send).toBeCalledTimes(1)
-      expect(link.send).toHaveBeenLastCalledWith('tray:action', 'setShortcut', 'Test', {
-        enabled: true,
-        configuring: false,
-        modifierKeys: ['Meta'],
-        shortcutKey: 'Slash'
-      })
-    })
-  })
->>>>>>> Choose summon keybinding (#1494)
 })
