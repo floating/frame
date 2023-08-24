37871af0fde196d308e2a9f3d64557672c53e99a
Matt Holtzman <matt.holtzman@gmail.com>
2023-08-24 17:49:46 -0400

    allow chain metadata with no native currency rate data

--- main/store/state/types/rate.ts
+++ main/store/state/types/rate.ts
@@ -5,6 +5,7 @@ const v37 = z
     price: z.number(),
     change24hr: z.number()
   })
+  .default({ price: 0, change24hr: 0 })
   // remove stale price data
   .transform((rate) => ({ ...rate, price: 0, change24hr: 0 }))
 
