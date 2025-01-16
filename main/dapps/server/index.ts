8a1353e89986eb9a37a6874e3c1a43b72044f058
Matt Holtzman <matt.holtzman@gmail.com>
2025-01-16 16:17:39 -0500

    fix parse import

--- main/dapps/server/index.ts
+++ main/dapps/server/index.ts
@@ -1,5 +1,5 @@
 import http from 'http'
-import cookie from 'cookie'
+import { parse } from 'cookie'
 import { URL } from 'url'
 import { hash } from 'eth-ens-namehash'
 
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
   const url = new URL(req.url || '', `http://${req.headers.host}`)
   const ens = url.hostname.replace('.localhost', '')
   const namehash = hash(ens)
-  const session = (req.headers.cookie && cookie.parse(req.headers.cookie).__frameSession) || ''
+  const session = (req.headers.cookie && parse(req.headers.cookie).__frameSession) || ''
 
   // check if dapp is added before progressing
   if (!store('main.dapps', namehash)) {
